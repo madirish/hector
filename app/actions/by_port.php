8eae33f6e1efa660e0947073069c5b894bd202cc
Justin C. Klein Keane <jukeane@sas.upenn.edu>
2012-10-19 10:14:41 -0400

    Added any, all and exclude to port search function.

--- app/actions/by_port.php
+++ app/actions/by_port.php
@@ -41,9 +41,11 @@ if ($anyports != 0 || $allports != 0) {
 		  		'nsr.nmap_scan_result_timestamp ' .
 		  		'from nmap_scan_result nsr, state s, host h ';
 		  if (! (isset($appuser) && $appuser->get_is_admin())) { $query .= ', user_x_support_group ux ';}		
-		  $query .= ' LEFT OUTER JOIN host_x_tag x on x.host_id = h.host_id ';
-			if ($tagsin != '') $query .= 'AND x.tag_id IN (' . $tagsin . ') ';	
-			if ($tagsex != '') $query .= 'and (x.tag_id NOT IN (' . $tagsex . ') OR nsr.host_id NOT IN (select host_id from host_x_tag)) ';
+		  if ($tagsin != '' || $tagsex != '') {
+		  	$query .= ' LEFT OUTER JOIN host_x_tag x on x.host_id = h.host_id ';
+		  	if ($tagsin != '') $query .= ' AND x.tag_id IN (' . $tagsin . ') ';
+		  	if ($tagsex != '') $query .= ' AND x.tag_id NOT IN (' . $tagsex . ') ';
+		  }
 			$query .= 'WHERE nsr.host_id = h.host_id AND ' .
 					'h.host_id = nsr.host_id and ' .
 					's.state_id = nsr.state_id AND ' .
@@ -53,6 +55,8 @@ if ($anyports != 0 || $allports != 0) {
 				$query .= ' AND ux.supportgroup_id = h.supportgroup_id and ' .
 					'ux.user_id = ' . $appuser->get_id() . ' ';
 			}
+			if ($tagsex != '') $query .= ' AND x.host_id IS NULL';
+			if ($tagsin != '') $query .= ' AND x.host_id IS NOT NULL ';
 			$query .= ' order by h.supportgroup_id';
 			$host_results = $db->fetch_object_array($query);
 		}
@@ -62,6 +66,8 @@ if ($anyports != 0 || $allports != 0) {
 			$query .= ' left outer join nmap_scan_result nsr2 on nsr1.host_id = nsr2.host_id ';
 			if ($tagsin != '' || $tagsex != '') {
 				$query .= ' LEFT OUTER JOIN host_x_tag x on nsr1.host_id = x.host_id ';
+		  	if ($tagsin != '') $query .= ' AND x.tag_id IN (' . $tagsin . ') ';
+		  	if ($tagsex != '') $query .= ' AND x.tag_id NOT IN (' . $tagsex . ') ';
 			}
 			$query .= ' where nsr1.nmap_scan_result_port_number IN (' . $anyports . ') ';
 			$query .= ' AND nsr1.state_id = 1 AND nsr2.nmap_scan_result_port_number IN (' . $portsex . ') ';
@@ -76,7 +82,8 @@ if ($anyports != 0 || $allports != 0) {
 			$host_results = $db->fetch_object_array($query);
 		}
 	}
-	else if ($anyports == 0 || $allports != 0) { 
+	else if ($anyports == 0 && $allports != 0) { 
+		// ToDo:  Add access restrictions and tag exclusion/inclusions
 		$content .= '<h3>Results with all ports: ' . $allports;
 		if ($portsex != 0) $content .= ' excluding ports: ' . $portsex;
 		$content .= '</h3>';
@@ -113,6 +120,46 @@ if ($anyports != 0 || $allports != 0) {
 				$exportscount++;
 				$i++;
 			}
+		}
+		$host_results = $db->fetch_object_array($query);
+	}
+	else { // Got all the criteria
+		$content .= '<h3>Results with all ports: ' . $allports;
+		$content .= ' with any ports: ' . $anyports;
+		if ($portsex != 0) $content .= ' excluding ports: ' . $portsex;
+		$content .= '</h3>';
+		
+		// Select the machines with any of the requested ports, minus exclusion
+		$query = ' select distinct(nsr1.host_id) from nmap_scan_result nsr1 ';
+		if ($portsex != 0) {
+			$query .= ' LEFT JOIN nmap_scan_result nsrex ON nsr1.host_id = nsrex.host_id ';
+			$query .= ' AND nsrex.nmap_scan_result_port_number IN (' . $portsex . ') ';
+			$query .= ' AND nsrex.state_id = 1 ';
+		}
+		$query .= ' where nsr1.nmap_scan_result_port_number IN (' . $anyports . ') and nsr1.state_id = 1';
+		if ($portsex != 0) {
+			$query .= ' AND nsrex.host_id IS NULL';
+		}
+		$hosts = $db->fetch_object_array($query);
+		$host_ids = '';
+		foreach($hosts as $host) $host_ids .= $host->host_id . ',';
+		$host_ids = substr($host_ids, 0, -1); // Chop off the last comma
+		
+		// Now get the intersection of that list and the hosts with allports
+		$ports = explode(',', $allports);
+		$ports = array_map(intval, $ports);  // type safety
+		$count = count($ports);
+		$i = 1;
+		$query = 'select nsr1.host_id from nmap_scan_result nsr1 ';
+		while ($i < $count) {
+			$query .= ' INNER JOIN nmap_scan_result nsr' . $i++ . ' ON nsr1.host_id = nsr' . $i . '.host_id ';
+		}
+		$i = 1;	
+		$query .= ' where nsr1.host_id IN ('. $host_ids . ') AND ';
+		$query .= ' nsr1.nmap_scan_result_port_number = ' . $ports[0];
+		$query .= ' AND nsr1.state_id = 1 ';
+		while ($i < $count) {
+			$query .= ' AND nsr' . $i++ . '.nmap_scan_result_port_number = ' . $ports[$i-1];
 		}
 		$host_results = $db->fetch_object_array($query);
 	}

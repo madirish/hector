af660dc55c7afcbd9183dccdae2b1d5c0ad8d66c
Justin C. Klein Keane <jukeane@sas.upenn.edu>
2013-02-19 14:02:11 -0500

    Added some space to the footer

--- app/templates/footer.tpl.php
+++ app/templates/footer.tpl.php
@@ -1,4 +1,5 @@
 </div> <!-- end content div -->
+<br/><br/>
 <div class="well well-small"><strong>HECTOR</strong>, an Open Souce Security Platform, by the <a href="http://www.sas.upenn.edu" title="Website for the University of Pennsylvania School of Arts & Sciences">University of Pennsylvania School of Arts &amp; Sciences</a></div>
 
 </div> <!-- end container div -->

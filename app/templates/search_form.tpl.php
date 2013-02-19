8b06fcbe0e90c220f7e2b437cbcd89292c71725f
Justin C. Klein Keane <jukeane@sas.upenn.edu>
2013-02-19 09:09:04 -0500

    Updated to toggle advanced search form

--- app/templates/search_form.tpl.php
+++ app/templates/search_form.tpl.php
@@ -1,15 +1,28 @@
-<fieldset>
-<legend>Search</legend>
-<p>Search is <em>inclusive</em> meaning results are drawn from machines 
-that match any of the specified criteria.</p>
-<form method="post" name="<?php echo $formname;?>" id="<?php echo $formname;?>">
-<table>
-	<tr><td>Hostname:</td><td><input type="text" name="hostname"/></td></tr>
-  <tr><td>IP:</td><td><input type="text" name="ip"/></td></tr>
-  <tr><td>Service version (e.x. "OpenSSH"):</td><td><input type="text" name="version"/></td></tr>
-	<tr><td>&nbsp;</td><td><input type="submit" value="Search"/></td></tr>
-</table>
-<input type="hidden" name="token" value="<?php echo $token;?>"/>
-<input type="hidden" name="form_name" value="<?php echo $formname;?>"/>
-</form>
-</fieldset>
+<div class="accordion" id="search-accordion">
+	<div class="accordion-group">
+		<div calss="accordion-heading">
+		<a class="accordion-toggle" data-toggle="collapse" data-parent="#search-accordion" href="#collapseOne">
+		Advanced Search
+		</a>
+		</div>
+		<div id="collapseOne" class="accordion-body collapse in">
+			<div class="accodion-inner">
+				<fieldset>
+				<legend>Search</legend>
+				<p>Search is <em>inclusive</em> meaning results are drawn from machines 
+				that match any of the specified criteria.</p>
+				<form method="post" name="<?php echo $formname;?>" id="<?php echo $formname;?>">
+				<table>
+					<tr><td>Hostname:</td><td><input type="text" name="hostname"/></td></tr>
+				  <tr><td>IP:</td><td><input type="text" name="ip"/></td></tr>
+				  <tr><td>Service version (e.x. "OpenSSH"):</td><td><input type="text" name="version"/></td></tr>
+					<tr><td>&nbsp;</td><td><input type="submit" value="Search"/></td></tr>
+				</table>
+				<input type="hidden" name="token" value="<?php echo $token;?>"/>
+				<input type="hidden" name="form_name" value="<?php echo $formname;?>"/>
+				</form>
+				</fieldset>
+			</div>
+		</div>
+	</div>
+</div>

0dc4ad89ef17773ac9960cc86b725fec7893336f
Justin C. Klein Keane <jukeane@sas.upenn.edu>
2013-05-08 09:30:27 -0400

    Added link for malicious IP database search

--- app/templates/admin_headers.tpl.php
+++ app/templates/admin_headers.tpl.php
@@ -39,6 +39,7 @@ $ip_search->save();
       <ul class="dropdown-menu">
         <li><a href="?action=detection">Detection summary</a></li>
         <li><a href="?action=honeypot">Honeypot data</a></li>
+        <li><a href="?action=attackerip">Malicious IP database</a></li>
       </ul>
     </li>
     <li class="dropdown">
@@ -66,7 +67,7 @@ $ip_search->save();
     <li><a href="?action=logout">Log Out</a></li>
   </ul>
     
-    <form class="navbar-search pull-right" method="post" name="<?php echo $ip_search_name;?>" id="<?php echo $ip_search_name;?>" action="/?action=assets&object=search">
+    <form class="navbar-search pull-right" method="post" name="<?php echo $ip_search_name;?>" id="<?php echo $ip_search_name;?>" action="?action=assets&object=search">
     	<input type="text" class="search-query" placeholder="Search for IP" name="ip">
     	<input type="hidden" name="token" value="<?php echo $ip_search_token;?>"/>
 			<input type="hidden" name="form_name" value="<?php echo $ip_search_name;?>"/>

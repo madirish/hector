29a87ed1c9d81049c117e6721351dedd28112116
Justin C. Klein Keane <jukeane@sas.upenn.edu>
2013-07-17 11:07:01 -0400

    Updated with some minor code style tweaks and removed the ajax check.

--- app/actions/kojoney2_feed.php
+++ app/actions/kojoney2_feed.php
@@ -6,27 +6,25 @@
  */
 require_once($approot . 'lib/class.Api_key.php');
 require_once($approot . 'lib/class.Db.php');
-if(!(isset($_GET['api_key']) and isset($_GET['ajax'])))
-{
-	header("Location: ?action=summary");
+
+// Redirect to the main page if no API key
+if(! isset($_GET['api_key'])) {
+	header("Location: " . $_SESSION['site_url']);
 }
-else
-{
+else {
 	header("Content-type: text/plain");
-	$api_key=new Api_key();
-	$isvalid=$api_key->validate($_GET['api_key']);
-	if($isvalid)
-	{
+	$api_key = new Api_key();
+	if($api_key->validate($_GET['api_key'])) {
 		print "#Kojoney2 feed\n";
 		print "#description: Kojoney2 login attempts\n";
 		print "#values: address, detecttime\n";
 		print "#delimiters: \\t, \\n\n\n";
 		$db = Db::get_instance();
 		$sql = 'select distinct(ip), time from koj_login_attempts ' .
-			'where time between date_sub(curdate(), interval 1 day) and curdate() group by ip';
+			'where time between date_sub(curdate(), interval 1 day) ' .
+			'and curdate() group by ip';
 		$results = $db->fetch_object_array($sql);
-		foreach($results as $result)
-		{
+		foreach($results as $result) {
 			print $result->ip . "\t" . $result->time."\n";
 		}
 		$db->close();

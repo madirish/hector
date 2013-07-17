1fd068d8d2f478ad4deea39f7942aa54361455ff
Justin C. Klein Keane <jukeane@sas.upenn.edu>
2013-07-17 11:53:30 -0400

    Restricted actions to only valid feeds to prevent auth bypass.

--- html/feeds/index.php
+++ html/feeds/index.php
@@ -15,20 +15,9 @@ $templates = $approot . 'templates/';
 require_once($approot . 'lib/class.Config.php');
 new Config();
 
-/**
- * Begin program flow control.  Build an array
- * of valid actions so we can hand off control 
- * from the GET variable without having to
- * worry about null byte injection.
- * Ref:  http://www.madirish.net/?article=436
- */
-$actions = array();
-if (! $files = opendir($approot . '/actions')) {
-	die("Error opening actions directory.  Please contact a system administrator.");
-}
-while (($dir = readdir($files)) !== false) {
-	if (substr($dir, -4) == ".php") $actions[] = substr($dir, 0, -4);
-}
+// Array of valid feeds
+$actions = array('kojoney2_feed');
+
 
 // Set up the action subcontroller or abort
 if (isset($_GET['action']) && in_array($_GET['action'], $actions)) {

d44cbe477613a52c6812ecd3f595e295f7443d39
Justin C. Klein Keane <jukeane@sas.upenn.edu>
2013-07-26 10:41:17 -0400

    Added chart output.

--- app/templates/summary.tpl.php
+++ app/templates/summary.tpl.php
@@ -6,14 +6,37 @@ $(document).ready(function()
     } 
 ); 
 </script>
-
+<script src="js/Chart.js" type="text/javascript"></script>
+<script src="js/jquery.js" type="text/javascript"></script>
 <p class="lead"><?php echo $count;?> Hosts Tracked</p>
 
 
 <div class="row">
   <div class="span6">
-
-	<h3>Scanner:  Top Ports Detected</h3>
+  	<h3>Scanner:  Top Ports Detected</h3>
+	<canvas id="topPortsChart" width="400"></canvas>
+	<script>
+	var data = {
+	  labels : [<?php
+	  	foreach ($port_result as $row) {
+	  		print '"' . $row->port_number . '",';
+	  	} 
+	  ?>],
+	  datasets : [ 
+			{ 
+				fillColor : "rgba(220,220,220,0.2)",
+				strokeColor : "rgba(220,220,220,1)",
+				data : [<?php
+					foreach ($port_result as $row) {
+				  		print $row->portcount . ',';
+				  	}
+				?>]
+			}
+		]
+	}
+	var myNewChart = new Chart(document.getElementById("topPortsChart").getContext("2d")).Bar(data);
+	</script>
+	
 	<table class="table table-striped" id="top-ports-dectected">
 	<thead>
 	<tr><th>#</th><th>Port Number</th><th>Hosts with port open</th></tr>
@@ -33,6 +56,28 @@ $(document).ready(function()
 
 	</div><div class="span6">
 	<h3>Darknet:  Top Port Probes in Last 4 Days</h3>
+	<canvas id="darknetChart"  width="400"></canvas>
+	<script>
+	var data = {
+	  labels : [<?php
+	  	foreach ($probe_result as $row) {
+	  		print '"' . $row->port . '",';
+	  	} 
+	  ?>],
+	  datasets : [ 
+			{ 
+				fillColor : "rgba(220,220,220,0.2)",
+				strokeColor : "rgba(220,220,220,1)",
+				data : [<?php
+					foreach ($probe_result as $row) {
+				  		print $row->cnt . ',';
+				  	}
+				?>]
+			}
+		]
+	}
+	var myNewChart = new Chart(document.getElementById("darknetChart").getContext("2d")).Bar(data);
+	</script>
 	<table class="table table-striped table-condensed" id="darknet-probes-summary">
 	<thead>
 	<tr><th>#</th><th>Port Number</th><th>Total darknet probes</th></tr>

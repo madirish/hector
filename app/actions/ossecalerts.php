0284535bee7d9a2484fa4572a221af4ab5c3c6d1
Justin Klein Keane <jukeane@sas.upenn.edu>
2013-08-23 12:51:48 -0400

    Initial commit of OSSEC alerts view

---
+++ app/actions/ossecalerts.php
@@ -0,0 +1,21 @@
+<?php
+/**
+ * This is the default subcontroller for the 
+ * OSSEC clients report
+ * 
+ * @package HECTOR
+ * @author Justin C. Klein Keane <jukeane@sas.upenn.edu>
+ */
+require_once($approot . 'lib/class.Db.php');
+
+$db = Db::get_instance();
+
+$sql = 'SELECT a.*, r.rule_level, r.rule_message ' .
+		'FROM ossec_alerts a, ossec_rules r ' .
+		'WHERE r.rule_id = a.rule_id ' .
+		'ORDER BY a.alert_date ' .
+		'LIMIT 100';
+$alerts = $db->fetch_object_array($sql);
+
+include_once($templates. 'admin_headers.tpl.php');
+include_once($templates . 'ossecalerts.tpl.php');

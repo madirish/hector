4fade398667ec7dff31b2c024ecd3fbaaeeded29
Justin Klein Keane <jukeane@sas.upenn.edu>
2013-08-27 07:25:33 -0400

    Major rework of SQL and code to enhance readability.

--- app/actions/global.php
+++ app/actions/global.php
@@ -1,31 +1,40 @@
 <?php
 /**
- *  controller to manage global variables
- *  by Josh Bauer <joshbauer3@gmail.com>
+ * Utility page to manage global variables for the interface
+ *  
+ * @author Josh Bauer <joshbauer3@gmail.com>
+ * @author Justin C. Klein Keane <jukeane@sas.upenn.edu>
+ * @package HECTOR
+ * @version 2013.08.28
  */
 require_once($approot . 'lib/class.Db.php');
 global $appuser;
+$db = Db::get_instance();
 
 //badge that contains the number of vulnerabilities that are not ignored or fixed
-global $vuln_badge;
+$vuln_badge = '';
+
 if (! isset($appuser)) {
 	if (! isset($_SESSION['user_id'])) die("<h2>Fatal error!<?h2>User not initialized.");
 	else $appuser = new User($_SESSION['user_id']);
 }
-$sql = 'select count(*) as count from (select vd.vuln_details_id from vuln_details vd inner join vuln_x_host vh on vd.vuln_details_id=vh.vuln_details_id ';
+$sql = 'SELECT COUNT(x.vuln_details_id) AS vulncount ' .
+		'FROM vuln_details d, vuln_details_x_host x, host h ' .
+		'WHERE x.vuln_details_id = d.vuln_details_id AND ' .
+			'h.host_id = x.host_id AND ' .
+			'd.vuln_details_ignore = 0 AND ' .
+			'd.vuln_details_fixed = 0';
 if (isset($appuser) && ! $appuser->get_is_admin()) {
-			// Using this object via the web
-			$sql .= 'inner join host h on h.host_id = vh.host_id ';
-			$sql .= 'inner join user_x_supportgroup us on us.supportgroup_id = h.supportgroup_id ';
-			$sql = array(
-				$sql . 'where vd.vuln_details_ignore = 0 and vd.vuln_details_fixed = 0 and us.user_id = ?i group by vd.vuln_details_text) as temp_table',
-				$appuser->get_id());
+			// Limit reports to hosts the user is responsible for
+			$sql .= ' AND x.host_id = h.host_id ' .
+					' AND h.supportgroup_id IN (' . implode(',', $appuser->get_supportgroup_ids()) . ')';
 }
-else {
-			$sql .= 'where vd.vuln_details_ignore = 0 and vd.vuln_details_fixed = 0 group by vd.vuln_details_text) as temp_table';
-}
-$db = Db::get_instance();
+
 $result = $db->fetch_object_array($sql);
-$vuln_count = $result[0]->count;
-$vuln_badge = ($vuln_count > 0) ? '<span class="badge">' . $vuln_count . '</span>' : ''
+if (isset($result[0])) {
+	$vuln_count = $result[0]->vulncount;
+	$vuln_badge = ($vuln_count > 0) ? '<span class="badge badge-important">' . $vuln_count . '</span>' : '';
+}
+
+$reports = ($vuln_count > 0) ? '<span class="noticeme">Reports</span>' : 'Reports';
 ?>

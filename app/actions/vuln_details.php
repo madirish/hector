ec0809ad26f636406474de2456db8fca6f396dca
Justin Klein Keane <jukeane@sas.upenn.edu>
2013-08-27 23:59:06 -0400

    updated with new documentation.

--- app/actions/vuln_details.php
+++ app/actions/vuln_details.php
@@ -3,13 +3,19 @@
  * This is the default subcontroller for vulnerability
  * details
  * 
- * by Josh Bauer <joshbauer3@gmail.com>
- * 
+ * @author Josh Bauer <joshbauer3@gmail.com>
+ * @author Justin C. Klein Keane <jukeane@sas.upenn.edu>
+ * @package HECTOR
+ * @version 2013.08.29
  */
-include_once($templates. 'admin_headers.tpl.php');
+
+// Necessary includes
 require_once($approot . 'lib/class.Vuln_detail.php');
-if (isset($_GET['id']) && ($_GET['id'] != '')) {
-	$vuln_details= new Vuln_detail(intval($_GET['id']));
-	include_once($templates . 'vuln_details.tpl.php');
-}
-?>
+
+$vuln_id = isset($_GET['id']) ? intval($_GET['id']) : '';
+$vuln_details= new Vuln_detail($vuln_id);
+
+include_once($templates. 'admin_headers.tpl.php');
+include_once($templates . 'vuln_details.tpl.php');
+
+?> 

7ac1a0246cf1afee8bb380f8e0d60545a3efae1f
Justin Klein Keane <jukeane@sas.upenn.edu>
2013-08-28 07:02:39 -0400

    Added documentation

--- app/lib/interface.Maleable_Object_Interface.php
+++ app/lib/interface.Maleable_Object_Interface.php
@@ -1,5 +1,11 @@
 <?php
-
+/**
+ * interface.Maleable_Object_Interface.php
+ *
+ * @abstract This interface is intended to allow for standard API throughout the application.
+ * @author Justin C. Klein Keane <jukeane@sas.upenn.edu>
+ * @package HECTOR
+ */
 interface Maleable_Object_Interface {
 	public function delete();
 	public function get_add_alter_form();

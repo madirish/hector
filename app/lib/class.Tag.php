b6f47104d51cb21ef7f43e66f12e7c301f4776b8
Justin Klein Keane <jukeane@sas.upenn.edu>
2013-08-28 17:00:35 -0400

    Updated documentation, add set_id

--- app/lib/class.Tag.php
+++ app/lib/class.Tag.php
@@ -33,42 +33,61 @@ class Tag extends Maleable_Object implements Maleable_Object_Interface {
 
 
     // --- ATTRIBUTES ---
+    /**
+     * Instance of the Db
+     * 
+     * @access private
+     * @var Db An instance of the Db
+     */
+    private $db = null;
 
     /**
-     * Short description of attribute id
+     * Unique ID from the data layer
      *
-     * @access private
-     * @var int
+     * @access protected
+     * @var int Unique id
      */
     protected $id = null;
 
 	/**
 	 * Tag name
 	 * 
-	 * @var String
+	 * @access private
+	 * @var String The name of the tag
 	 */
     private $name;
+    
+    /**
+     * Instance of the Log
+     * 
+     * @access private
+     * @var Log An instance of the Log
+     */
+    private $log = null;
 
 	/**
 	 * Hosts associated with this tag.  This
 	 * is just a convenience (for reporting).
 	 * There is no interface for altering this
 	 * attribute.
+	 * 
+	 * @access public
+	 * @var Array The host_ids for Host objects
 	 */
     public $host_ids = array();
 
     // --- OPERATIONS ---
 
     /**
-     * Short description of method __construct
+     * Construct a new blank Tag or instantiate one
+     * from the data layer based on ID
      *
      * @access public
      * @author Justin C. Klein Keane <jukeane@sas.upenn.edu>
-     * @param  int id
+     * @param  Int The unique ID of the Tag
      * @return void
      */
-    public function __construct($id = '')
-    {
+    public function __construct($id = '') {
         $this->db = Db::get_instance();
 		$this->log = Log::get_instance();
 		if ($id != '') {
@@ -77,8 +96,8 @@ class Tag extends Maleable_Object implements Maleable_Object_Interface {
 				$id
 			);
 			$result = $this->db->fetch_object_array($sql);
-			$this->id = $result[0]->tag_id;
-			$this->name = $result[0]->tag_name;
+			$this->set_id($result[0]->tag_id);
+			$this->set_name($result[0]->tag_name);
 			$sql = array(
 				'SELECT host_id FROM host_x_tag WHERE tag_id = ?i',
 				$id
@@ -114,9 +133,11 @@ class Tag extends Maleable_Object implements Maleable_Object_Interface {
 	/**
 	 * This is a functional method designed to return
 	 * the form associated with altering a tag.
+	 * 
+	 * @access public
+	 * @return Array The array for the default CRUD template.
 	 */
 	public function get_add_alter_form() {
-
 		return array (
 			array('label'=>'Tag name',
 					'type'=>'text',
@@ -129,7 +150,7 @@ class Tag extends Maleable_Object implements Maleable_Object_Interface {
     /**
      *  This function directly supports the Collection class.
 	 *
-	 * @return SQL select string
+	 * @return String SQL select string
 	 */
 	public function get_collection_definition($filter = '', $orderby = '') {
 		$query_args = array();
@@ -150,49 +171,87 @@ class Tag extends Maleable_Object implements Maleable_Object_Interface {
 		return $sql;
 	}
 
+	/**
+	 * Get the displays for the default details template
+	 * 
+	 * @return Array Dispalays for default template
+	 */
 	public function get_displays() {
 		return array('Name'=>'get_name');
 	}
 
     /**
-     * Short description of method get_id
+     * Get the unique ID for the object
      *
      * @access public
      * @author Justin C. Klein Keane <jukeane@sas.upenn.edu>
-     * @return int
+     * @return Int The unique ID of the object
      */
-    public function get_id()
-    {
-       return $this->id;
+    public function get_id() {
+       return intval($this->id);
     }
 
+	/**
+	 * The HTML safe name of the Tag
+	 * 
+	 * @access public
+	 * @return String The HTML display safe name of the Tag.
+	 */
     public function get_name() {
-		return $this->name;
+		return htmlspecialchars($this->name);
     }
 
-    public function save() {if ($this->id > 0 ) {
+	/**
+	 * Persist the Tag to the data layer
+	 * 
+	 * @access public
+	 * @return Boolean True if everything worked, FALSE on error.
+	 */
+    public function save() {
+    	$retval = FALSE;
+    	if ($this->id > 0 ) {
     		// Update an existing user
 	    	$sql = array(
 	    		'UPDATE tag SET tag_name = \'?s\' WHERE tag_id = \'?i\'',
 	    		$this->get_name(),
 	    		$this->get_id()
 	    	);
-	    	$this->db->iud_sql($sql);
+	    	$retval = $this->db->iud_sql($sql);
     	}
     	else {
     		$sql = array(
 				'INSERT INTO tag SET tag_name = \'?s\'',
     			$this->get_name()
 	    	);
-	    	$this->db->iud_sql($sql);
+	    	$retval = $this->db->iud_sql($sql);
+	    	// Now set the id
+	    	$sql = 'SELECT LAST_INSERT_ID() AS last_id';
+	    	$result = $this->db->fetch_object_array($sql);
+	    	if (isset($result[0]) && $result[0]->last_id > 0) {
+	    		$this->set_id($result[0]->last_id);
+	    	}
     	}
+    	return $retval;
+    }
+    
+    /**
+     * Set the id attribute.
+     * 
+     * @access protected
+     * @param Int The unique ID from the data layer
+     */
+    protected function set_id($id) {
+    	$this->id = intval($id);
     }
 
+	/**
+	 * Set the name of the Tag
+	 * 
+	 * @access public
+	 * @param String The name of the tag
+	 */
     public function set_name($name) {
-    	if ($name != '')
-    		$this->name = htmlspecialchars($name);
-    	elseif ($name == '')
-    		$this->name = '';
+    	$this->name = $name;
     }
 
 } /* end of class Tag */

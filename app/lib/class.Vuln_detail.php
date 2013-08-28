faf13fcef67db512f4cc46c1b0c1e9f1078efcd8
Justin Klein Keane <jukeane@sas.upenn.edu>
2013-08-27 23:58:21 -0400

    Updated to include new docs, removed cruft, and added new utility functions.

--- app/lib/class.Vuln_detail.php
+++ app/lib/class.Vuln_detail.php
@@ -14,7 +14,7 @@ if (0 > version_compare(PHP_VERSION, '5')) {
 }
 
 /* user defined includes */
-require_once('class.Config.php');
+require_once('class.Vuln.php');
 require_once('class.Db.php');
 require_once('class.Log.php');
 require_once('class.Collection.php');
@@ -33,135 +33,124 @@ class Vuln_detail extends Maleable_Object implements Maleable_Object_Interface {
     // --- ATTRIBUTES ---
 
     /**
-     * vuln_detail_id
+     * The unique ID from the database.
      *
      * @access private
      * @var int
      */
     protected $id = null;
-    
-    /**
-     * array of vuln_detail_id's
-     * that match this vulnerability
-     *
-     * @access private
-     * @var array
-     */
-    protected $ids = null;
 
 	/**
-	 * vuln_detail_text
+	 * Text of the vulnerability detail, usually indicating 
+	 * the specific circumstances of the vulnerability or 
+	 * any additional details useful for remediation.
 	 * 
+	 * @access private
 	 * @var String
 	 */
 	private $text;
-	
-	/**
-	 * first logged datetime
-	 * 
-	 * @var String
-	 */
-    private $first_datetime;
     
     /**
-	 * last logged datetime
+	 * Boolean indicating that the vulnearbility should be 
+	 * ignored (usually in case of mitigations).
 	 * 
-	 * @var String
-	 */
-    private $last_datetime;
-    
-    /**
-	 * ignore
-	 * 
+	 * @access private
 	 * @var Boolean
 	 */
 	private $ignore;
 	
 	/**
-	 * fixed
+	 * The timestamp of when the vulnerability was marked to 
+	 * be ignored.
 	 * 
+	 * @access private
+	 * @var datetime
+	 */
+	private $ignored_datetime;
+	
+	/**
+	 * The unique ID, for creating a User object, of the user
+	 * who makred the vulnerability for being ignored.
+	 * 
+	 * @access private
+	 * @var int
+	 */
+	private $ignored_user_id;
+	
+	/**
+	 * Boolean indicating whether or not the vulnerability has
+	 * been fixed.  Default is to 0 (or false).
+	 * 
+	 * @access private
 	 * @var Boolean
 	 */
 	private $fixed;
 	
 	/**
-	 * fixed datetime
+	 * The datetime when the vulnerability was marked as 
+	 * fixed.
 	 * 
+	 * @access private
 	 * @var String
 	 */
 	private $fixed_datetime;
 	
 	/**
-	 * fixed notes
+	 * Notes entered by whomever fixed the vulnerability, to
+	 * indicate steps taken or additional details.
 	 * 
-	 * @var String
+	 * @access private
+	 * @var String Notes associated with the fix.
 	 */
 	private $fixed_notes;
+	
+	/** 
+	 * Unique ID, for creating a User object, of the user who
+	 * marked the vulnerability as fixed.
+	 * 
+	 * @access private
+	 * @var int The user id for who marked fixed
+	 */
+	private $fixed_user_id;
 
     /**
-     * vuln_id
+     * Instance of the corresponding Vuln object
      *
      * @access private
-     * @var int
+     * @var Object Vuln 
      */
-    private $vuln_id = null;
-    
-   /**
-     * vuln_name
-     *
-     * @access private
-     * @var int
-     */
-    private $vuln_name;
+    private $vuln = null;
     
     /**
-     * vuln_description
-     *
+     * The unique ID of the assocaited Vulnerability, which
+     * can be used to instantiate a Vuln object.
+     * 
      * @access private
-     * @var int
+     * @var int The unique vuln id
      */
-    private $vuln_description;
+    private $vuln_id = 0;
     
     /**
-     * vuln_cve
+     * The unique ID of the associated Host so that we can 
+     * instantiate a Host object to get more details.
      *
      * @access private
-     * @var int
-     */
-    private $vuln_cve;
-    
-    /**
-     * vuln_cve
-     *
-     * @access private
-     * @var int
-     */
-    private $vuln_osvdb;
-    
-    /**
-     * host_id
-     *
-     * @access private
-     * @var int
+     * @var int Unique host id
      */
     private $host_id = null;
-    
-    /**
-     * host_name
-     *
-     * @access private
-     * @var int
-     */
-    private $host_name = null;
     
     // --- OPERATIONS ---
 
     /**
-     * Short description of method __construct
-     *
+     * Constructor method, to pull data about the object from 
+     * the data layer, or construct the skeletal object so it
+     * can later be persisted back to the data layer.
+     * 
      * @access public
      * @author Josh Bauer <joshbauer3@gmail.com>
-     * @param  int id
+     * @author Justin C. Klein Keane <jukeane@sas.upenn.edu>
+     * @version 2013.08.29
+     * @param  int Unique id for the Vuln_detail
      * @return void
      */
     public function __construct($id = '')
@@ -170,49 +159,24 @@ class Vuln_detail extends Maleable_Object implements Maleable_Object_Interface {
 		$this->log = Log::get_instance();
 		if ($id != '') {
 			$sql = array(
-				'SELECT vd.*, v.vuln_name, v.vuln_description, v.vuln_cve, v.vuln_osvdb, vh.host_id, h.host_name FROM vuln_detail vd ' .
-				'inner join vuln v on v.vuln_id = vd.vuln_id ' .
-				'inner join vuln_detail_x_host vh on vh.vuln_detail_id = vd.vuln_detail_id ' . 
-				'inner join host h on h.host_id = vh.host_id WHERE vd.vuln_detail_id =?i',
+				'SELECT * from vuln_detail where vuln_detail_id = ?i',
 				$id
 			);
 			$result = $this->db->fetch_object_array($sql);
-			$this->id = $result[0]->vuln_detail_id;
-			$this->text = $result[0]->vuln_detail_text;
-			$this->ignore = $result[0]->vuln_detail_ignore;
-			$this->fixed = $result[0]->vuln_detail_fixed;
-			$this->fixed_datetime = $result[0]->vuln_detail_fixed_datetime;
-			$this->fixed_notes = $result[0]->vuln_detail_fixed_notes;
-			$this->vuln_id = $result[0]->vuln_id;
-			$this->vuln_name = $result[0]->vuln_name;
-			$this->vuln_description = $result[0]->vuln_description;
-			$this->vuln_cve = $result[0]->vuln_cve;
-			$this->vuln_osvdb = $result[0]->vuln_osvdb;
-			$result = $this->db->fetch_object_array($sql);
-			$this->host_id = $result[0]->host_id;
-			$this->host_name = $result[0]->host_name;
-			
-			$ids = array();
-			$sql = array(
-				'SELECT vd.vuln_detail_id from vuln_detail vd ' .
-				'inner join vuln_detail_x_host vh on vh.vuln_detail_id = vd.vuln_detail_id ' . 
-				'inner join host h on h.host_id = vh.host_id ' .
-				'WHERE vh.host_id=?i and vd.vuln_id=?i and vd.vuln_detail_text=\'?s\'',
-				$this->host_id,
-				$this->vuln_id,
-				$this->text
-				);
-			$results = $this->db->fetch_object_array($sql);
-			foreach($results as $result) $this->ids[]=$result->vuln_detail_id;
-			
-			$sql = array(
-				'SELECT max(vd.vuln_detail_datetime) as last, min(vd.vuln_detail_datetime) as first from vuln_detail vd ' .
-				'WHERE vd.vuln_detail_id in (?s)',
-				implode(',', $this->ids)
-				);
-			$result = $this->db->fetch_object_array($sql);
-			$this->first_datetime = $result[0]->first;
-			$this->last_datetime = $result[0]->last;
+			if (isset($result[0])) {
+				$r = $result[0];
+				$this->id = $r->vuln_detail_id;
+				$this->text = $r->vuln_detail_text;
+				$this->ignore = $r->vuln_detail_ignore;
+				$this->ignored_datetime = $r->vuln_detail_ignored_datetime;
+				$this->ignored_user_id = $r->vuln_detail_ignoredby_user_id;
+				$this->fixed = $r->vuln_detail_fixed;
+				$this->fixed_datetime = $r->vuln_detail_fixed_datetime;
+				$this->fixed_notes = $r->vuln_detail_fixed_notes;
+				$this->fixed_user_id = $r->vuln_detail_fixedby_user_id;
+				$this->vuln_id = $r->vuln_id;
+				$this->host_id = $r->host_id;
+			}
 		}
     }
 
@@ -226,7 +190,7 @@ class Vuln_detail extends Maleable_Object implements Maleable_Object_Interface {
     public function delete() {
     	if ($this->id > 0 ) {
     		$sql=array('Delete FROM vuln_detail WHERE vuln_detail_id =?i',
-    			$this->get_id()
+    			$this->id
     		);
     		$this->db->iud_sql($sql);
     	}
@@ -237,7 +201,10 @@ class Vuln_detail extends Maleable_Object implements Maleable_Object_Interface {
 	 * the form associated with altering vuln_detail information.
 	 * 
 	 * NOT USED IN THIS CLASS
-	 *
+	 * 
+	 * @access public
+	 * @return Array A zero element array.
+	 * @deprecated
 	 */
 	public function get_add_alter_form() {
 		return array ();
@@ -246,11 +213,12 @@ class Vuln_detail extends Maleable_Object implements Maleable_Object_Interface {
     /**
      *  This function directly supports the Collection class.
 	 *
-	 * @return SQL select string
+	 * @access public
+	 * @return String SQL select string
 	 */
 	public function get_collection_definition($filter = '', $orderby = '') {
 		$query_args = array();
-		$sql = 'SELECT vd.vuln_detail_id FROM vuln_detail vd WHERE vd.vuln_detail_id > 0';
+		$sql = 'SELECT vuln_detail_id FROM vuln_detail WHERE vuln_detail_id > 0';
 		if ($filter != '' && is_array($filter))  {
 			$sql .= ' ' . array_shift($filter);
 			$sql = $this->db->parse_query(array($sql, $filter));
@@ -262,7 +230,7 @@ class Vuln_detail extends Maleable_Object implements Maleable_Object_Interface {
 			$sql .= ' ' . $orderby;
 		}
 		else if ($orderby == '') {
-			$sql .= ' ORDER BY vd.vuln_detail_id';
+			$sql .= ' ORDER BY vuln_detail_datetime DESC';
 		}
 		return $sql;
 	}
@@ -273,13 +241,9 @@ class Vuln_detail extends Maleable_Object implements Maleable_Object_Interface {
 	public function get_displays() {
 		return array();
 	}
-
-	public function get_first_datetime() {
-		return $this->first_datetime;
-    }
     
     public function get_fixed() {
-		return $this->fixed;
+		return (bool) $this->fixed;
     }
     
     public function get_fixed_datetime() {
@@ -287,57 +251,62 @@ class Vuln_detail extends Maleable_Object implements Maleable_Object_Interface {
     }
     
     public function get_fixed_notes() {
-       return $this->fixed_notes;
+       return htmlspecialchars($this->fixed_notes);
+    }
+    
+    public function get_fixed_user_id() {
+    	return intval($this->fixed_user_id);
     }
     
     public function get_host_id() {
-		return $this->host_id;
+		return intval($this->host_id);
     }
     
-    public function get_host_name() {
-		return $this->host_name;
+    public function get_host() {
+		if ($this->host == NULL) {
+			$this->host = new Host($this->host_id);
+		}
+		return $this->host;
     }
     
     public function get_id() {
-		return $this->id;
+		return intval($this->id);
     }
       
     public function get_ignore() {
-		return $this->ignore;
+		return (bool) $this->ignore;
     }
     
-    public function get_last_datetime() {
-		return $this->last_datetime;
+    public function get_ignore_datetime() {
+    	return $this->ignore_datetime;
+    }
+    
+    public function get_ignored_user_id() {
+    	return intval($this->ignored_user_id);
     }
     
     public function get_text() {
-		return $this->text;
+		return htmlspecialchars($this->text);
     }
     
-     public function get_vuln_cve() {
-		return $this->vuln_cve;
+    public function get_vuln() {
+    	if ($this->vuln == NULL) {
+    		$this->vuln = new Vuln($this->vuln_id);
+    	}
+    	return $this->vuln;
     }
     
-    public function get_vuln_description() {
-		return $this->vuln_description;
-    }
-    
-    public function get_vuln_id() {
-		return $this->vuln_id;
-    }
-    
-     public function get_vuln_name() {
-		return $this->vuln_name;
-    }
-    
-     public function get_vuln_osvdb() {
-		return $this->vuln_osvdb;
-    }
-    
-    public function save() {if ($this->id > 0 ) {
+    public function save() {
+    	if ($this->id > 0 ) {
     		// Update an existing vuln_detail
 	    	$sql = array(
-	    		'UPDATE vuln_detail SET vuln_detail_text = \'?s\', vuln_detail_ignore = \'?s\', vuln_detail_fixed = \'?s\', vuln_detail_fixed_datetime = \'?s\', vuln_detail_fixed_notes =\'?s\' ' .
+	    		'UPDATE vuln_detail SET ' .
+	    			'vuln_detail_text = \'?s\', ' .
+	    			'vuln_detail_ignore = \'?s\', ' .
+	    			'vuln_detail_fixed = \'?s\', ' .
+	    			'vuln_detail_fixed_datetime = \'?s\', ' .
+	    			'vuln_detail_fixed_notes =\'?s\',' .
+	    			'vunl_detail_ignore ' .
 	    		'WHERE vuln_detail_id in (?s)',
 				$this->get_text(),
 	    		$this->get_ignore(),
@@ -351,10 +320,7 @@ class Vuln_detail extends Maleable_Object implements Maleable_Object_Interface {
     }
     
      public function set_fixed($fixed) {
-    	if ($fixed != '')
-    		$this->fixed = htmlspecialchars($fixed);
-    	elseif ($fixed == '')
-    		$this->fixed = '';
+     	$this->fixed = (bool) $fixed;
     }
     
     public function set_fixed_datetime($fixed_datetime) {
@@ -372,10 +338,7 @@ class Vuln_detail extends Maleable_Object implements Maleable_Object_Interface {
     }
     
     public function set_ignore($ignore) {
-    	if ($ignore != '')
-    		$this->ignore = htmlspecialchars($ignore);
-    	elseif ($ignore == '')
-    		$this->ignore = '';
+    	$this->ignore = (bool) $ignore;
     }
     
     public function set_text($text) {
@@ -383,6 +346,10 @@ class Vuln_detail extends Maleable_Object implements Maleable_Object_Interface {
     		$this->text = htmlspecialchars($text);
     	elseif ($text == '')
     		$this->text = '';
+    }
+    
+    public function set_vuln_id($id) {
+    	$this->vuln_id = intval($id);
     }
 
 } /* end of class Vuln_detail */

dc834fca1ba5b5ef5c31fc5167e48a8c1a489fda
Justin Klein Keane <jukeane@sas.upenn.edu>
2013-08-28 07:00:50 -0400

    updated documentation

--- app/lib/class.Maleable_Object.php
+++ app/lib/class.Maleable_Object.php
@@ -1,40 +1,52 @@
 <?php
-
+/**
+ * class.Maleable_Object.php
+ *
+ * @abstract This [singleton] class is intended to allow for logging throughout the application.
+ * @author Justin C. Klein Keane <jukeane@sas.upenn.edu>
+ * @package HECTOR
+ */
 class Maleable_Object {
 
     /**
-     * Short description of attribute id
+     * The unique id from the data layer.
      *
      * @access protected
-     * @var int
+     * @var int Unique id from the data layer
      */
     protected $id = 0;
 
     /**
-     * Short description of method get_id
+     * Return the unique id from the data layer
      *
      * @access public
      * @author Justin C. Klein Keane, <jukeane@sas.upenn.edu>
-     * @return int
+     * @return int The unique id from the data layer.
      */
     public function get_id()
     {
         return (int) $this->id;
     } 
 	
-	
+	/**
+	 * Process the add/edit form with the proper
+	 * callback function.
+	 * 
+	 * @author Justin C. Klein Keane <jukeane@sas.upenn.edu>
+	 * @access public
+	 * @param String Callback function to process values
+	 * @param String Value to pass to the callback function.
+	 */
 	public function process_form($callback, $value) {
 		$this->$callback($value);
 	}
 	
-	
-
     /**
-     * Short description of method set_id
+     * Set the object's unique id
      *
      * @access protected
      * @author Justin C. Klein Keane <jukeane@sas.upenn.edu>
-     * @param  int id
+     * @param  int The unique id for hte object
      * @return void
      */
     protected function set_id($id) {

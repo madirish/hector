9cbc2fdc56a32fda6cca058ed4667b5b2eb91689
Justin Klein Keane <jukeane@sas.upenn.edu>
2013-09-02 00:04:30 -0400

    Added some more checks

--- app/tests/class.Host.test.php
+++ app/tests/class.Host.test.php
@@ -41,6 +41,76 @@ class TestOfHostClass extends UnitTestCase {
 		$this->assertEqual($altnames[0], $althostname);
 	}
 	
+	function testOS() {
+		$os = 'Test';
+		$this->host->set_os($os);
+		$this->assertEqual($this->host->get_os(), $os);
+    $this->assertFalse($this->host->get_os() == 'ThisIsNotTheOS');
+	}
+    
+  function testTechnical() {
+  	$tech = 'Joe Support Dude';
+    $this->host->set_technical($tech);
+    $this->assertEqual($this->host->get_technical(), $tech);
+    $this->assertNotEqual($this->host->get_technical(), '');
+  }
+    
+  function testLink() {
+    $link = "http://www.example.com";
+    $this->host->set_link($link);
+    $this->assertEqual($this->host->get_link(), $link);
+    $this->assertFalse($this->host->get_link() == 'http://ThisIsNotTheLink.com');
+    $this->assertFalse($this->host->set_link('ThisIsNotAValidLink'));
+  }
+  
+  function testHostGroups() {
+    $hg = array(1,2,3);
+    $this->host->set_host_group_ids($hg);
+    $this->assertEqual($this->host->get_host_group_ids(), $hg);	
+    $this->assertEqual(count($this->host->get_host_group_ids()), 3);
+  }
+  
+  function testTags() {
+    $tag = array(1,2,3);
+    $this->host->set_tag_ids($tag);
+    $this->assertEqual($this->host->get_tag_ids(), $tag); 
+    $this->assertEqual(count($this->host->get_tag_ids()), 3);
+  }
+  
+  function testNote() {
+  	$note = 'This is just a test note.';
+    $this->host->set_note($note);
+    $this->assertEqual($this->host->get_note(), $note);
+    $htmlnote = '<script>alert("xss note");</script>';
+    $this->host->set_note($htmlnote);
+    $this->assertNotEqual($this->host->get_note(), $htmlnote);
+  }
+  
+  function testPolicy() {
+  	$this->host->set_policy(TRUE);
+    $this->assertTrue($this->host->get_policy());
+    $this->host->set_policy(FALSE);
+    $this->assertFalse($this->host->get_policy());
+  }
+  
+  function testSponsor() {
+  	$sponsor = 'Edward Test';
+    $this->host->set_sponsor($sponsor);
+    $this->assertEqual($this->host->get_sponsor(), $sponsor);
+    $this->assertNotEqual($this->host->get_sponsor(), '');
+  }
+  
+  function testLocation() {
+  	$this->location = new Location();
+    $this->location->set_name('Test');
+    $this->location->save();
+    $id = $this->location->get_id();
+    $this->assertTrue($this->host->set_location_id($id));
+    $this->assertEqual($this->host->get_location_id(), $id);
+    $this->assertEqual($this->host->get_location_name(), 'Test');
+    $this->location->delete();
+  }
+	
 	function testGetAddAlterForm() {
 		$this->assertIsA($this->host->get_add_alter_form(), 'Array');
 	}

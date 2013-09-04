d71b8fd1bf2110aac9e568545c2d06dd8450fd45
Justin C. Klein Keane <jukeane@sas.upenn.edu>
2013-09-04 13:16:32 -0400

    Added more tests for the Alert class.

--- app/tests/class.Alert.test.php
+++ app/tests/class.Alert.test.php
@@ -3,9 +3,55 @@
 require_once(dirname(__FILE__) . '/../software/simpletest/autorun.php');
 require_once(dirname(__FILE__) . '/../lib/class.Alert.php');
 
+
 class TestOfAlertClass extends UnitTestCase {
+	
+  private $alert;
+  
+  private $alert_id;
+  
+  function setUp() {
+  	$this->alert = new Alert();
+  }
+
   function testAlertConstructor() {
-  	$alert = new Alert();
-  	$this->assertIsA($alert, 'Alert');
+  	$this->assertIsA($this->alert, 'Alert');
+  }
+  
+  function testAlertSave() {
+  	$this->assertNull($this->alert->get_id());
+  	$this->alert->set_host_id(1);
+  	$this->alert->set_string('Test string');
+  	$this->assertTrue($this->alert->save());
+  	$this->alert_id = $this->alert->get_id();
+  	$this->assertTrue($this->alert_id > 0);
+  }
+  
+  function testSetAlertHostId() {
+  	$this->alert->set_host_id(1);
+  	$this->assertTrue($this->alert->get_host_id() == 1);
+  }
+  
+  function testSetAlertString() {
+  	$testval = 'Test string';
+  	$this->alert->set_string($testval);
+  	$this->assertEqual($this->alert->get_string(), $testval);
+  }
+  
+  function testAlertTimeStamp() {
+  	$this->alert = new Alert();
+  	$this->alert->set_host_id(1);
+  	$this->alert->set_string('test string');
+  	$this->alert->save();
+  	$id = $this->alert->get_id();
+  	$secondAlert = new Alert($id);
+  	$this->assertTrue($secondAlert->get_timestamp() > 0);
+  }
+  
+  function testAlertDelete() {
+  	$id = $this->alert_id;
+  	$this->alert->delete();
+  	$secondAlert = new Alert($id);
+  	$this->assertNull($this->alert->get_id());
   }
 }

3b42b317d4120d30fdef509cf6c9ad5b0bbd173b
Josh Bauer <jobauer@sas.upenn.edu>
2013-09-09 12:46:47 -0400

    ncrack scan is in a working state

--- app/scripts/ncrack_scan.php
+++ app/scripts/ncrack_scan.php
@@ -1,12 +1,19 @@
 <?php
 /**
- * NCrack scan.
+ * Ncrack scan.
  * 
+ * For full help on usage see show_ncrack_help() below.  
+ * Example usage:
+ * 
+ * $ php ncrack_scan.php -p=telnet,ssh:24 -g=1,4 
+ * 
+ * @author Josh Bauer <joshbauer3@gmail.com>
  * @author Justin C. Klein Keane <jukeane@sas.upen.edu>
  * @package HECTOR
- * @todo Fix the display description
+ * @todo Remove testing output
+ * @todo Filter hosts by open ports
  */
-
+ 
 /**
  * Defined vars
  */
@@ -27,19 +34,32 @@ require_once($approot . 'lib/class.Scan_type.php');
 // Make sure of the environment
 global $add_edit;
 if(php_sapi_name() != 'cli') {
-	
+	$servicelist='';
+	if (isset($_GET['id'])) {
+		$id = intval($_GET['id']);
+		$scan = new Scan_type($id);
+		$flags = $scan->get_flags();
+		$flags = explode('-', $flags);
+		foreach ($flags as $flag) {
+			if (substr($flag, 0,1)=='p') $servicelist = substr($flag, 2);
+		}
+	}
 	$is_executable[] = array('ncrack_scan.php' => 'ncrack scan');
 	global $javascripts;
 	$javascripts[] = <<<EOT
 	<script type="text/javascript">
 		function ncrack_display() {
-			var ncrackHTML = "<p>NCRACK    Xprobe2 uses port data from the database to determine operating systems and update host records.</p/>";
-			ncrackHTML += "<p>xprobe2  is  an  active  operating system fingerprinting tool with a different approach to operating system";
-			ncrackHTML += "fingerprinting. xprobe2 relies on fuzzy signature matching, probabilistic guesses, multiple matches  simul-";
-			ncrackHTML += "taneously, and a signature database.</p>";
-			ncrackHTML += "<p>The  operation  of  xprobe2  is  described in a paper titled \"xprobe2 - A ´Fuzzy´ Approach to Remote Active";
-			ncrackHTML += "Operating System Fingerprinting\".</p>";
+			var ncrackHTML = "<p>NCRACKNcrack is a high-speed network authentication cracking tool.</p>";
+			ncrackHTML += "<p> Protocols supported include RDP, SSH, http(s), SMB, pop3(s), VNC, FTP, and telnet.</p>";
+			ncrackHTML += "Services to scan (comma delimited): <input type='text' id='servicelist' onBlur='updateServices()' value='$servicelist'/>";
+			ncrackHTML += "<small> Ex. 'telnet,SSH:24'</small>"
 			document.getElementById("specs").innerHTML = ncrackHTML;
+		}
+		
+		function updateServices() {
+			var ports =  document.getElementById("servicelist").value
+			if (ports=='') document.getElementById("flags").value = '';
+			else document.getElementById("flags").value = "-p=" + ports;
 		}
 		// Fire this up as it's the default
 		ncrack_display();
@@ -68,29 +88,98 @@ else {
 		die("Can't find ncrack executable at $ncrack.  Check your config.ini.\n");
 	}
 	ncrack_loggit("ncrack_scan.php status", "ncrack_scan.php invoked.");
-	$hosts = array('192.168.56.1' => 1, '192.168.56.101' => 2, '192.168.56.102' => 3, '192.168.56.103' => 4, '192.168.56.104' => 5, '192.168.56.105' => 6, '192.168.56.106' => 7);
+	
+	//set the defaults
+	$groups = null;
+	$services = null;
+	
+	/**
+	 * This will be an associative array of the form
+	 * host_ip => Host object
+	 */
+	$hosts = array();
+	
+	/**
+	 * This is an array of the 10 most common usernames from kojoney2.
+	 * These usernames will be used as the username list for the scan.
+	 */
 	$usernames = array();
-	$passwords = array();	
-	$sql = 'select distinct(username) as uname, count(id) as ucount from koj_login_attempt group by username order by ucount desc limit 10';
+	
+	/**
+	 * This is an array of the 10 most common passwords from kojoney2.
+	 * These passwords will be used as the password list for the scan.
+	 */
+	$passwords = array();	 
+	
+	// Parse through the command line arguments
+	foreach ($argv as $arg) {
+		if (substr($arg, -15) == 'ncrack_scan.php') continue;
+		$flag = substr(strtolower($arg),0,2);
+		if ($flag == '-g') $groups = substr($arg,strpos($arg,'=')+1);
+		if ($flag == '-p') $services = substr($arg,strpos($arg,'=')+1);
+	}
+	
+	// Determine host groups
+	if ($groups != NULL) {
+		$groups = mysql_real_escape_string($groups);
+		$host_groups = new Collection('Host_group', 'AND host_group_id IN(' . $groups .')');
+		if (isset($host_groups->members) && is_array($host_groups->members)) {
+			foreach($host_groups->members as $host_group) {
+				foreach ($host_group->get_host_ids() as $host_id) {
+					$newhost = new Host($host_id);
+					$hosts[$newhost->get_ip()] = $newhost;
+				}
+			}
+		}
+	}
+	else {
+		// just grab all the hosts
+		$allhosts = new Collection('Host');
+		if (isset($allhosts->members) && is_array($allhosts->members)) {
+			foreach ($allhosts->members as $newhost) {
+				$hosts[$newhost->get_ip()] = $newhost;
+			}
+		}
+	}
+	$sql = 'select ' . 
+				'distinct(username) as uname, ' .
+				'count(id) as ucount ' .
+			'from koj_login_attempt ' . 
+			'group by username ' .
+			'order by ucount desc limit 10';
 	$results= $db->fetch_object_array($sql);
 	foreach($results as $result) $usernames[] = $result->uname;
-	$sql = 'select distinct(password) as passwd, count(id) as pcount from koj_login_attempt group by passwd order by pcount desc limit 10';
+	$sql = 'select ' .
+				'distinct(password) as passwd, ' .
+				'count(id) as pcount ' .
+			'from koj_login_attempt ' .
+			'group by passwd ' .
+			'order by pcount desc limit 10';
 	$results= $db->fetch_object_array($sql);
 	foreach($results as $result) $passwords[] = $result->passwd;
-	$command = $ncrack . ' -p telnet --user ' . implode(',', $usernames) . ' --pass ' . implode(',', $passwords) . ' 192.168.56.101-106';
+	$command = $ncrack;
+	$command .= ' -p ' . $services;
+	$command .= ' --user ' . implode(',', $usernames);
+	$command .= ' --pass ' . implode(',', $passwords);
+	$command .= ' ' . implode(' ', array_keys($hosts));
 	print $command . "\r\n\r\n";
 	$output = shell_exec($command);
 	print_r($output);
 	if(preg_match_all("/(\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3}) (\d+\/\w+) (\w+)\: \'(.+)\' \'(.+)\'/", $output, $matches, PREG_SET_ORDER)) {
 		print_r($matches);
 		foreach($matches as $match) {
-			$text = 'Easily guessed credentials for service: ' . $match[3] . ' on port: ' . $match[2] . ' with credentials: (' . $match[4] . ':' . $match[5] . ')';
-			$sql = 'insert into vuln_detail set vuln_id=1, vuln_detail_text=\''.$text.'\'';
-			$db->iud_sql($sql);
-			$vd_id = $db->fetch_object_array('select LAST_INSERT_ID() as id from vuln_detail limit 1');
-			print_r($vd_id);
-			$sql = array('insert into vuln_x_host set host_id=?i, vuln_detail_id=?i', $hosts[$match[1]], $vd_id[0]->id);
-			print_r($sql);
+			$text = 'Easily guessed credentials for service: ' . $match[3] . 
+					' on port: ' . $match[2] . 
+					' with credentials: (' . $match[4] . ':' . $match[5] . ')';
+			$sql = array(
+					'insert into vuln_detail set '.
+						'vuln_id=?i, ' .
+						'vuln_detail_text=\'?s\', '.
+						'host_id=?i',
+					1,
+					$text,
+					$hosts[$match[1]]->get_id()
+					);
 			$db->iud_sql($sql);
 		}
 	}
@@ -101,5 +190,19 @@ function ncrack_loggit($status, $message) {
 	global $dblog;
 	$log->write_message($message);
 	$dblog->log($status, $message);
+}
+
+function show_ncrack_help($error) {
+	echo "Usage: ncrack_scan.php [arguments=params]\n";
+  	echo $error;
+	echo "\n\n";
+	echo "Arguments:\n";
+	echo "-g\tHost groups id's to scan\n";
+	echo "-p\tService(s) to scan\n";
+	echo "\n\nExample Usage:\n";
+	echo '$ php ncrack_scan.php -p=telnet,ssh:24 -g=1,4' . "\n";
+	echo "Would scan for hosts in the 'web servers' and 'critical hosts' groups (id 1 & 4) \n";
+	echo "for weak credentials in telnet and ssh on port 24 and store the results in the database.\n\n";
+	//exit;
 }
 ?>

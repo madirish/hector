2565f049a97977b053e20abbc183a02f788de0da
Justin Klein Keane <jukeane@sas.upenn.edu>
2013-09-11 08:16:54 -0400

    Updated to remove redundant constructors.

--- app/tests/class.Tag.test.php
+++ app/tests/class.Tag.test.php
@@ -18,34 +18,28 @@ class TestOfTagClass extends UnitTestCase {
 	}
 	
 	function testTagId() {
-		$this->tag = new Tag();
 		$this->assertEqual($this->tag->get_id(), 0);
 	}
 	
 	function testTagName() {
-		$this->tag = new Tag();
 		$name = 'Test';
 		$this->tag->set_name($name);
 		$this->assertEqual($this->tag->get_name(), $name);
 	}
 	
 	function testGetAddAlterForm() {
-		$this->tag = new Tag();
 		$this->assertIsA($this->tag->get_add_alter_form(), 'Array');
 	}
 	
 	function testGetCollectionDefinition() {
-		$this->tag = new Tag();
 		$this->assertIsA($this->tag->get_collection_definition(), 'String');
 	}
 	
 	function testGetDisplays() {
-		$this->tag = new Tag();
 		$this->assertIsA($this->tag->get_displays(), 'Array');
 	}
 	
 	function testSaveDelete() {
-		$this->tag = new Tag();
 		$this->tag->set_name('Test');
 		$this->assertTrue($this->tag->save());
 		$this->assertTrue($this->tag->get_id() > 0 );

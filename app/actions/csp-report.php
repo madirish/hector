57591f908ca491d6188c578ae0a669f3ca94f27f
Justin Klein Keane <jukeane@sas.upenn.edu>
2013-09-11 15:06:59 -0400

    Initial commit of HTML 5 content security policy handler.

---
+++ app/actions/csp-report.php
@@ -0,0 +1,18 @@
+<?php
+/**
+ * Reporting API for HTML 5 Content Security Policy (CSP) violations.
+ */
+
+$file = fopen(dirname(__FILE__) . '/../logs/csp-report.txt', 'a');
+$json = file_get_contents('php://input');
+
+$csp = json_decode($json, true);
+if (isset($csp['csp-report'])) {
+	foreach ($csp['csp-report'] as $key => $val) {
+	    fwrite($file, $key . ': ' . $val . PHP_EOL);
+	}
+}
+
+fwrite($file, 'End of report.' . PHP_EOL . PHP_EOL);
+fclose($file);
+?>

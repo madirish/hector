d4b3ceef75b6f07e8d65c80ce628327b0259b2f7
Justin Klein Keane <jukeane@sas.upenn.edu>
2013-09-12 14:24:45 -0400

    Included instance of Log since Form->log is now protected to fix access violation error.

--- app/actions/form_preprocess.php
+++ app/actions/form_preprocess.php
@@ -11,17 +11,19 @@
  * Necessary includes
  */
 require_once($approot . 'lib/class.Form.php');
+require_once($approot . 'lib/class.Log.php');
 
 $form = new Form();
+$log = Log::get_instance();
 
 if (! (isset($_POST['form_name']) || isset($_POST['token'])) ) {
-	$form->log->write_error('XSRF attack detected!');
+	$log->write_error('XSRF attack detected!');
 	print "Possible XSRF detected!";
 	die();
 }
 
 if (! $form->validate($_POST['form_name'], $_POST['token'], $_SERVER['REMOTE_ADDR'])) {
-	$form->log->write_error('Possible XSRF attack, form did not validate.');
+	$log->write_error('Possible XSRF attack, form did not validate.');
 	// Wipe out all the values
 	foreach ($_POST as $key=>$val) {
 		$_POST[$key] = '';

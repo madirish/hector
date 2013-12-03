351db9dba8ded1f979f9f74e2d8a980291f3cb7c
Justin C. Klein Keane <jukeane@sas.upenn.edu>
2013-12-03 15:28:21 -0500

    Initial commit of the test cases

---
+++ app/tests/class.IRAsset.test.php
@@ -0,0 +1,45 @@
+<?php
+require_once(dirname(__FILE__) . '/../software/simpletest/autorun.php');
+require_once(dirname(__FILE__) . '/../lib/class.IRAsset.php');
+
+
+class TestOfLocationClass extends UnitTestCase {
+	
+	function setUp() {
+		$this->asset = new IRAsset();
+	}
+	
+	function tearDown() {
+		$this->asset->delete();
+	}
+	
+	function testLocationClass() {
+		$this->assertIsA($this->asset, 'IRAsset');
+	}
+	
+	function testId() {
+		$this->assertEqual($this->asset->get_id(), 0);
+	}
+	
+	function testName() {
+		$name = 'Test';
+		$this->asset->set_name($name);
+		$this->assertEqual($this->asset->get_name(), $name);
+	}
+	
+	function testGetAddAlterForm() {
+		$this->assertIsA($this->asset->get_add_alter_form(), 'Array');
+	}
+	
+	function testGetCollectionDefinition() {
+		$this->assertIsA($this->asset->get_collection_definition(), 'String');
+	}
+	
+	function testSaveDelete() {
+		$this->asset->set_name('Test');
+		$this->assertTrue($this->asset->save());
+		$this->assertTrue($this->asset->get_id() > 0 );
+		$this->assertTrue($this->asset->delete());
+	}
+}
+?>

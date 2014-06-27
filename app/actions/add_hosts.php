b628cc64b27d818367fb253c7a93eae9b468359b
Justin C. Klein Keane <jukeane@sas.upenn.edu>
2014-06-27 13:17:47 -0400

    Encapsulated database code in new class and updated business logic in controller to use object.

--- app/actions/add_hosts.php
+++ app/actions/add_hosts.php
@@ -17,91 +17,37 @@
  * Requre the Form for XSRF protection
  */ 
 require_once($approot . 'lib/class.Form.php');
-
 include_once($approot . 'lib/class.Collection.php');
 include_once($approot . 'lib/class.Host_group.php');
+require_once($approot . 'lib/class.BulkHostAdder.php');
 
 if (isset($_POST['startip'])) {
 	// Add hosts to the database
-	$startip = ip2long($_POST['startip']); 
-	$endip = isset($_POST['endip']) && $_POST['endip'] != '' ? ip2long($_POST['endip']) : $startip;
-	if ($endip < $startip) {
-		$message = "Start IP must be less than end IP.";
+	$adder = new BulkHostAdder();
+    
+	$hostgroups = array();
+	if (isset($_POST["newhostgroup"]) && $_POST["newhostgroup"] !== '') {
+		$group = new Host_group();
+		$group->set_name($_POST["newhostgroup"]);
+		$group->save();
+		$hostgroups[] = $group->get_id();
 	}
+	if (isset($_POST['hostgroup'])) {
+		if (is_array($_POST['hostgroup'])) {
+			foreach ($_POST['hostgroup'] as $group) {
+				$hostgroups[] = $group;
+			}	
+		}
+		else {
+			$hostgroups[] = $_POST['hostgroup'];
+		}
+	}
+	
+    if ($adder->addByIp($_POST['startip'], $_POST['endip'], $hostgroups)) {
+    	$message = "Hosts added.";
+    }
 	else {
-		$db = Db::get_instance();
-		$hostgroups = array();
-		if (isset($_POST["newhostgroup"]) && $_POST["newhostgroup"] !== '') {
-			$group = new Host_group();
-			$group->set_name($_POST["newhostgroup"]);
-			$group->save();
-			$hostgroups[] = $group->get_id();
-		}
-		if (isset($_POST['hostgroup'])) {
-			if (is_array($_POST['hostgroup'])) {
-				foreach ($_POST['hostgroup'] as $group) {
-					$hostgroups[] = $group;
-				}	
-			}
-			else {
-				$hostgroups[] = $_POST['hostgroup'];
-			}
-		}
-		$ip = $startip;
-		while ($ip <= $endip) {
-			// Don't save 192.168.2.0 for instance
-			// Probably a better mathy way to do this ($ip%8 == 0) ?
-			$ipstring = long2ip($ip);
-			if (substr(long2ip($ip), -2) != ".0") {
-				// Check for duplicate entries
-				$sql = array(
-					'SELECT host_id FROM host WHERE host_ip_numeric = inet_aton(\'?i\')',
-					$ipstring
-				);	
-				$result = $db->fetch_object_array($sql);
-				$id = isset($result[0]->host_id) ? $result[0]->host_id : 0;
-				
-				// If the host is new add it
-				if ($id < 1) {
-					$sql = array(
-						'INSERT INTO host SET host_ip = \'?s\', host_ip_numeric = inet_aton(\'?s\'), host_name = \'?s\'',
-						$ipstring, $ipstring, $ipstring
-					);
-					$db->iud_sql($sql);
-			    	// Now set the id
-			    	$id = mysql_insert_id();	
-			    	// Insert the host groups
-			    	foreach ($hostgroups as $group_id) {
-			    		$sql = array(
-							'INSERT INTO host_x_host_group SET host_id = ?i, host_group_id = ?i',
-							$id, $group_id
-						);
-						$db->iud_sql($sql);
-			    	}
-				}
-				// if this record already exists just set up the hostgruops
-				else {
-					foreach ($hostgroups as $group_id) {
-						$sql = array(
-							'SELECT host_group_id FROM host_group WHERE host_group_id = ?i AND host_id = ?i',
-							$group_id, $id
-						);	
-						$result = $db->fetch_object_array($sql);
-						$host_group_id = isset($result[0]->host_group_id) ? $result[0]->host_group_id : 0;
-						if ($host_group_id < 1) {
-							$sql = array(
-								'INSERT INTO host_x_host_group SET host_id = ?i, host_group_id = ?i',
-								$id, $group_id
-							);
-							$db->iud_sql($sql);
-						}
-					}
-					
-				}
-			}
-			$ip++; 
-		}
-		$message = "Hosts added.";
+		$message = $adder->getError();
 	}
 }
 

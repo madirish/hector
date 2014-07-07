fc77ce869eb3afa6c978e2cb6a806b2c0b22800c
Justin C. Klein Keane <jukeane@sas.upenn.edu>
2014-07-07 10:42:30 -0400

    Moving data table javascript elements around

--- app/templates/alerts.tpl.php
+++ app/templates/alerts.tpl.php
@@ -1,16 +1,5 @@
 <p class="lead">Port Change Alerts</p>
 
-<!-- DataTables CSS -->
-<link rel="stylesheet" type="text/css" href="//cdn.datatables.net/1.10.0/css/jquery.dataTables.css">
-  
-<!-- jQuery -->
-<script type="text/javascript" charset="utf8" src="//code.jquery.com/jquery-1.10.2.min.js"></script>
-  
-<!-- DataTables -->
-<script type="text/javascript" charset="utf8" src="//cdn.datatables.net/1.10.0/js/jquery.dataTables.js"></script>
-
-
-<!-- Output -->
 <table id="tablealerts" name="tablealerts" class="table table-striped table-bordered">
 <thead>
 <tr>

ea3a196770e3ad11647d090de0845055223db40a
Ubani Balogun <ubani@sas.upenn.edu>
2014-08-13 11:54:02 -0400

    Completed redesign of Hector ossecalerts table. Moved Sql into Ossec_Alerts and Ossec_Rule classes

--- app/templates/ossecalerts.tpl.php
+++ app/templates/ossecalerts.tpl.php
@@ -1,21 +1,12 @@
 <h2>OSSEC Alerts</h2>
-<?php print_r($ossec_rules)?>
+<strong><?php echo $record_count?> records in last week</strong>
 <div class="input-prepend input-append">
 	<span class="add-on">Minimum alert level:</span>
-	<select name="minlevel" class="input-mini">
-		<?php echo $leveloptions;?>
-	</select>
-	<span class="add-on">Start date:</span>
-	<input type="text" placeholder="<?php echo $startdateplaceholder;?>" name="startdate" class="input-small">
-	<span class="add-on">End date:</span>
-	<input type="text" placeholder="<?php echo $enddate;?>" name="enddate" class="input-small">
+	<input class="input-mini" name="minlevel" id="minlevel" type="text" pattern="\d*"  placeholder="0">
 	<span class="add-on">IP:</span>
-	<input type="text" placeholder="<?php echo $ip;?>" name="ip" class="input-small">
-	<button type="submit" class="btn">Apply filter</button>
+	<input type="text" placeholder="<?php echo $ipholder;?>" name="ip" class="input-small" id="ip">
+	<button type="button" class="btn" id="clearbtn">Clear filters</button>
 	</div>
-	<input type="hidden" name="token" value="<?php echo $filter_form_token;?>"/>
-	<input type="hidden" name="form_name" value="ossec_filter_form"/>
-	<a href="<?php echo $clearfilterurl;?>"><button type="button" class="btn">Clear filters</button></a>
 </div>
 <div class="row">
 <div class="span12">
@@ -46,71 +37,4 @@
 	</tbody>
 </table>
 </div>
-</div>	
-
-<strong><?php echo $thecount;?> records.</strong> 
-
-<!-- Sorter --> 
-<form method="post" name="ossec_filter_form" id="ossec_filter_form" action="<?php echo $href;?>&start=<?php echo $startrecord;?>">
-	<div class="input-prepend input-append">
-	<span class="add-on">Minimum alert level:</span>
-	<select name="minlevel" class="input-mini">
-		<?php echo $leveloptions;?>
-	</select>
-	<span class="add-on">Start date:</span>
-	<input type="text" placeholder="<?php echo $startdateplaceholder;?>" name="startdate" class="input-small">
-	<span class="add-on">End date:</span>
-	<input type="text" placeholder="<?php echo $enddate;?>" name="enddate" class="input-small">
-	<span class="add-on">IP:</span>
-	<input type="text" placeholder="<?php echo $ip;?>" name="ip" class="input-small">
-	<button type="submit" class="btn">Apply filter</button>
-	</div>
-	<input type="hidden" name="token" value="<?php echo $filter_form_token;?>"/>
-	<input type="hidden" name="form_name" value="ossec_filter_form"/>
-	<a href="<?php echo $clearfilterurl;?>"><button type="button" class="btn">Clear filters</button></a>
-</form>
-
-<!-- Pager -->
-<div class="pagination">
-    <ul>
-    <?php echo $pager;?>
-    </ul>
-</div>
-
-<!-- Output -->
-<table class="table table-striped table-bordered">
-<thead>
-	<tr>
-		<th>Date</th>
-		<th>Host</th>
-		<th>Source</th>
-		<th>Rule</th>
-		<th>Level</th>
-		<th>IP</th>
-		<th>Alert Message</th>
-	</tr>
-</thead>
-<tbody>
-<?php
-	if (isset($alerts) && is_array($alerts)) {
-		foreach ($alerts as $alert) {
-			echo '<tr><td>' . $alert->alert_date . '</td>'; 
-			echo '<td>' . $alert->host_id . '</td>'; 
-			echo '<td>' . $alert->alert_log . '</td>'; 
-			echo '<td>' . $alert->rule_message . '</td>'; 
-			echo '<td>' . $alert->rule_level . '</td>'; 
-			echo '<td>' . $alert->rule_src_ip . '</td>'; 
-			echo '<td>' . $alert->rule_log . '</td></tr>'; 
-			echo "\n";
-		}
-	}
-?>
-</tbody>
-</table>
-
-<!-- Pager -->
-<div class="pagination">
-    <ul>
-    <?php echo $pager;?>
-    </ul>
 </div>
--- html/js/ossecalerts.js
+++ html/js/ossecalerts.js
@@ -1,7 +1,52 @@
+$.fn.dataTable.ext.search.push(
+		function(settings, data, dataIndex ){
+			var min = parseInt($('#minlevel').val(),10);
+			var level = parseFloat(data[4]) || 0;
+			
+			if( isNaN(min) || min <= level){
+				return true;
+			}
+			return false;
+		}
+)
+
+$.fn.dataTable.ext.search.push(
+		function(settings, data, dataIndex){
+			var ip = $('#ip').val();
+			var regex = /\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3}/;
+			var res = ip.match(regex);
+			var ip_data = data[5] || 0;
+			
+			if (!ip || !res || ip == ip_data){
+				return true;
+			}
+			return false;
+		}
+)
+
 $(document).ready(function(){
-	$('#ossec-alerts-table').dataTable({
+	var table = $('#ossec-alerts-table').DataTable({
 		"sDom": '<"top"lf>rt<"bottom"ip>',
-		"order": [[0,"desc"]]
-			});
+		"order": [[0,"desc"]],
+		"oLanguage": {
+			"sSearch": "",
+			"sLengthMenu": "_MENU_ entries"
+		}
 	
+			});
+	$('.dataTables_filter input').attr("placeholder","Search")
+	
+	$('#minlevel').keyup(function(){
+		table.draw();
+	})
+	
+	$('#ip').keyup(function(){
+		table.draw();
+	})
+	
+	$('#clearbtn').click(function(){
+		$('#ip').val('');
+		$('#minlevel').val('');
+		table.draw();
+	})
 })

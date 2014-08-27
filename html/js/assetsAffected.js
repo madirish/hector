1c6e77a4b132ece153630aaa6111ede68a7f7a2d
Justin C. Klein Keane <jukeane@sas.upenn.edu>
2014-08-27 10:05:17 -0400

    Added DOM checks to ensure elements exist before manipulation or reference.

--- html/js/assetsAffected.js
+++ html/js/assetsAffected.js
@@ -8,26 +8,28 @@
 */
 
 $(document).ready(function(){
-	var assets_count = $.parseJSON($("#incident-assets-counts").text());
-	var labels = $.parseJSON($("#incident-assets-labels").text());
-	var colors = ["#F8FF01","#FF0F00","#69D2E7","#FF6600","#b0de09","#DDDDDD","#FFCC99","#DCB9B3","#C7604C","#C58EF8","#F9C7F5"];
-	var data = [];
-	
-	for (var i = 0; i < labels.length; i++){
-		var asset = labels[i];
-		var count = assets_count[asset]['count'];
-		data.push({value:count, color:colors[i], title:asset + " - " + count});
+	if (document.getElementById('incident-assets-counts') !== null) {
+		var assets_count = $.parseJSON($("#incident-assets-counts").text());
+		var labels = $.parseJSON($("#incident-assets-labels").text());
+		var colors = ["#F8FF01","#FF0F00","#69D2E7","#FF6600","#b0de09","#DDDDDD","#FFCC99","#DCB9B3","#C7604C","#C58EF8","#F9C7F5"];
+		var data = [];
+		
+		for (var i = 0; i < labels.length; i++){
+			var asset = labels[i];
+			var count = assets_count[asset]['count'];
+			data.push({value:count, color:colors[i], title:asset + " - " + count});
+		}
+		var ctx = $("#incident-assets-chart").get(0).getContext("2d");
+		ctx.canvas.width = 300;
+		ctx.canvas.height = 300;
+		var assets_chart = new Chart(ctx).Pie(data);
+		legend(document.getElementById("incident-assets-legend"),data);
+		
+		console.log(assets_count);
+		var i = 0;
+		$("#incident-assets-legend .title").each(function(index){
+			$(this).attr('onclick','location.href="' + assets_count[labels[i]]['href'] + '"');
+			i++;
+		});
 	}
-	var ctx = $("#incident-assets-chart").get(0).getContext("2d");
-	ctx.canvas.width = 300;
-	ctx.canvas.height = 300;
-	var assets_chart = new Chart(ctx).Pie(data);
-	legend(document.getElementById("incident-assets-legend"),data);
-	
-	console.log(assets_count);
-	var i = 0;
-	$("#incident-assets-legend .title").each(function(index){
-		$(this).attr('onclick','location.href="' + assets_count[labels[i]]['href'] + '"');
-		i++;
-	});
 })
--- html/js/incidentChart.js
+++ html/js/incidentChart.js
@@ -7,33 +7,35 @@
  * Pie Chart implementation for Hector summary page
  */
 $(document).ready(function(){
-	var chartHeader = JSON.parse(document.getElementById('incidentReportHeader').textContent);
-	console.log(chartHeader);
-	var chartLabels = JSON.parse(document.getElementById('incidentChartLabels').textContent);
-	var chartData = JSON.parse(document.getElementById('incidentChartCounts').textContent);
-	var colors = ["#F8FF01","#FF0F00","#69D2E7","#FF6600","#b0de09","#DDDDDD","#FFCC99","#D97041","#C7604C","#CCCFFF"];
-	var data = [];
-	
-	for (var i = 0; i < chartLabels.length; i++){
-		var label = chartLabels[i];
-		var count = chartData[label]['count'];
-		data.push({value:count , color:colors[i] ,title:label + " - " + count});
+	if (document.getElementById('incidentReportHeader') !== null) {
+		var chartHeader = JSON.parse(document.getElementById('incidentReportHeader').textContent);
+		console.log(chartHeader);
+		var chartLabels = JSON.parse(document.getElementById('incidentChartLabels').textContent);
+		var chartData = JSON.parse(document.getElementById('incidentChartCounts').textContent);
+		var colors = ["#F8FF01","#FF0F00","#69D2E7","#FF6600","#b0de09","#DDDDDD","#FFCC99","#D97041","#C7604C","#CCCFFF"];
+		var data = [];
+		
+		for (var i = 0; i < chartLabels.length; i++){
+			var label = chartLabels[i];
+			var count = chartData[label]['count'];
+			data.push({value:count , color:colors[i] ,title:label + " - " + count});
+		}
+		
+		document.getElementById("incidentChartHeader").innerHTML = chartHeader;
+		var ctx = document.getElementById("incidentChart").getContext("2d");
+		ctx.canvas.width = 300;
+		ctx.canvas.height = 300;
+		var incidentChart = new Chart(ctx).Pie(data);
+		legend(document.getElementById("incidentChartLegend"),data);
+		
+		for (i = 0; i < chartLabels.length; i++){
+			console.log($("#incidentChartLegend .title")[i]);
+		}
+		
+		var i = 0;
+		$("#incidentChartLegend .title").each(function(index){
+			$(this).attr('onclick', 'location.href="?action=incident_reports&threat_action=' + chartData[chartLabels[i]]['href'] + '"');
+			i++;
+		});
 	}
-	
-	document.getElementById("incidentChartHeader").innerHTML = chartHeader;
-	var ctx = document.getElementById("incidentChart").getContext("2d");
-	ctx.canvas.width = 300;
-	ctx.canvas.height = 300;
-	var incidentChart = new Chart(ctx).Pie(data);
-	legend(document.getElementById("incidentChartLegend"),data);
-	
-	for (i = 0; i < chartLabels.length; i++){
-		console.log($("#incidentChartLegend .title")[i]);
-	}
-	
-	var i = 0;
-	$("#incidentChartLegend .title").each(function(index){
-		$(this).attr('onclick', 'location.href="?action=incident_reports&threat_action=' + chartData[chartLabels[i]]['href'] + '"');
-		i++;
-	});
 })

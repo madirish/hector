1c189672772f21cdfb10207da1d32146e6e4da37
Justin C. Klein Keane <jukeane@sas.upenn.edu>
2014-08-27 09:28:54 -0400

    Fixed bug to check for existence of DOM elements before manipulation.

--- html/js/portSummaryChart.js
+++ html/js/portSummaryChart.js
@@ -1,20 +1,22 @@
 $(document).ready(function(){
-	var displayLabels = document.getElementById('portSummaryChartLabels').textContent.split(",");
-	var dataPoints = document.getElementById('portSummaryChartData').textContent.split(",");
-
-    var data = {
-        labels : displayLabels,
-        datasets : [ 
-            { 
-            fillColor : "rgba(120,220,220,0.2)",
-            strokeColor : "rgba(120,220,220,1)",
-            data : dataPoints
-            }
-        ]
-    }
-    var myNewChart = new Chart(document.getElementById("topPortsChart").getContext("2d")).Bar(data);
-	$("#topPortsChart").click(function (evt) {
-		var activeBars = myNewChart.getBarsAtEvent(evt);
-		location.href="?action=reports&report=by_port&ports=" + activeBars[0]["label"];
-	});    
+	if (document.getElementById('portSummaryChartLabels') !== null) {
+		var displayLabels = document.getElementById('portSummaryChartLabels').textContent.split(",");
+		var dataPoints = document.getElementById('portSummaryChartData').textContent.split(",");
+	
+	    var data = {
+	        labels : displayLabels,
+	        datasets : [ 
+	            { 
+	            fillColor : "rgba(120,220,220,0.2)",
+	            strokeColor : "rgba(120,220,220,1)",
+	            data : dataPoints
+	            }
+	        ]
+	    }
+	    var myNewChart = new Chart(document.getElementById("topPortsChart").getContext("2d")).Bar(data);
+		$("#topPortsChart").click(function (evt) {
+			var activeBars = myNewChart.getBarsAtEvent(evt);
+			location.href="?action=reports&report=by_port&ports=" + activeBars[0]["label"];
+		});
+	}
 });

93a270c63dca3d737cd02317c740d1f8c48a192a
Justin C. Klein Keane <jukeane@sas.upenn.edu>
2014-08-27 08:47:51 -0400

    Updated to do conditional includes of javascript and displays only if data exists.

--- app/templates/summary.tpl.php
+++ app/templates/summary.tpl.php
@@ -47,23 +47,31 @@ to perform automated scans of hosts.</p>
 <div class="row">
   <div class="span6">
   	<h3>Scanner:  Top Ports Detected</h3>
+  	<?php if ($portSummaryLabels !== '' && $portSummaryCounts !== '') { ?>
   	<div id="portSummaryChartLabels" class="hidden"><?php echo $portSummaryLabels;?></div>
   	<div id="portSummaryChartData" class="hidden"><?php echo $portSummaryCounts;?></div>
 	<canvas id="topPortsChart" height="300" width="550"></canvas>
-    
+	<?php } else { ?>
+		No ports detected.
+	<?php } ?>
   </div>
   
   <div class="span6">
 	<h3>Darknet:  Top Port Probes in Last 4 Days</h3>
+  	<?php if ($darknetSummaryLabels !== '' && $darknetSummaryCounts !== '') { ?>
 	<canvas id="darknetChart"  height="300" width="550"></canvas>
 	<div id="darknetSummaryChartLabels" class="hidden"><?php echo $darknetSummaryLabels;?></div>
   	<div id="darknetSummaryChartData" class="hidden"><?php echo $darknetSummaryCounts;?></div>
+	<?php } else { ?>
+		No port probes detected.
+	<?php } ?>
 	</div>
 </div>
 
 <div class="row">
     <!-- Incident Pie Chart -->
     <div class="span6"> 
+  		<?php if ($incidentchart_labels !== '[]' && $incidentchart_counts !== '[]') { ?>
         <div id="incident-div" class="chart-container">
             <div id="incidentReportHeader" class="hidden"><?php echo $incident_report_header?></div>
             <div id="incidentChartLabels" class="hidden"><?php echo $incidentchart_labels?></div>
@@ -71,20 +79,25 @@ to perform automated scans of hosts.</p>
             <h3 id="incidentChartHeader"></h3>
             <div id="incidentChartLegend"></div>
             <canvas id="incidentChart"></canvas>
-            
         </div>
+		<?php } else { ?>
+			No incidents reported.
+		<?php } ?>
     </div>
     
     <!-- Incident Assets Pie chart -->
 	<div class="span6">
+  		<?php if ($asset_count_json !== '[]' && $asset_labels_json !== '[]') { ?>
 		<div id="asset-chart-div" class="chart-container">
 			<h3 id="incident-assets-header"><?php echo $asset_count_header?></h3>
 			<div id="incident-assets-counts" class="hidden"><?php echo $asset_count_json?></div>
 			<div id="incident-assets-labels" class="hidden"><?php echo $asset_labels_json?></div>
 			<div id="incident-assets-legend"></div>
 			<canvas id="incident-assets-chart"></canvas>
-			
 		</div>
+		<?php } else { ?>
+			No incidents reported.
+		<?php } ?>
 	</div>
 
 
@@ -92,66 +105,65 @@ to perform automated scans of hosts.</p>
 
 <div class="row">
     <div class="span6">
-    <!-- Kojoney Login Attempts Map -->
-    <h3>Kojoney: Login Attempts in Last 7 Days</h3>
+   		<!-- Kojoney Login Attempts Map -->
+    	<h3>Kojoney2: Login Attempts in Last 7 Days</h3>
 		<div id="kojoney-map-counts" class="hidden"><?php echo $kojoneymapcounts ?></div>
 		<div id="kojoney-worldmap" style="height:300px;"></div>
     </div>
     
     	
 	<div class="span6">
-    <!-- Darknet Probes Map -->
-    <h3>Darknet:  Probes in Last 7 Days by Country</h3>
-    <div id="world-map-gdp" style="height:300px;"></div>
-    <script>
-    <?php
-    foreach ($darknetmapcounts as $key=>$val) {
-    	?>
-        countryData['<?php echo $key;?>']=<?php echo $val;?>;
-        <?php
-    }
-    ?>
-    //@code_start
-    $(function(){
-    $('#world-map-gdp').vectorMap({
-        map: 'world_mill_en',
-        series: {
-            regions: [{
-                values: countryData,
-                scale: ['#C8EEFF', '#0071A4'],
-                normalizeFunction: 'polynomial'
-            }]
-        },
-        onRegionLabelShow: function(event, label, code){
-            label.html(
-                '<b>'+label.html()+'</b></br>'+countryData[code]+ ' probes'
-            );
-        },
-        onRegionClick: function (event, code) {
-            location.href = "?action=darknetsummary&country="+code;
-        },
-        onRegionOver: function(event,label){
-			 $(this).css('cursor','pointer');
-		 },
-		 onRegionOut: function(event,label){
-			 $(this).css('cursor','default');
-		 }
-        });
-      });
-      //@code_end
-    </script>
+    	<!-- Darknet Probes Map -->
+	    <h3>Darknet:  Probes in Last 7 Days by Country</h3>
+	    <div id="world-map-gdp" style="height:300px;"></div>
+	    <script>
+	    <?php
+	    foreach ($darknetmapcounts as $key=>$val) {
+	    	?>
+	        countryData['<?php echo $key;?>']=<?php echo $val;?>;
+	        <?php
+	    }
+	    ?>
+	    //@code_start
+	    $(function(){
+	    $('#world-map-gdp').vectorMap({
+	        map: 'world_mill_en',
+	        series: {
+	            regions: [{
+	                values: countryData,
+	                scale: ['#C8EEFF', '#0071A4'],
+	                normalizeFunction: 'polynomial'
+	            }]
+	        },
+	        onRegionLabelShow: function(event, label, code){
+	            label.html(
+	                '<b>'+label.html()+'</b></br>'+countryData[code]+ ' probes'
+	            );
+	        },
+	        onRegionClick: function (event, code) {
+	            location.href = "?action=darknetsummary&country="+code;
+	        },
+	        onRegionOver: function(event,label){
+				 $(this).css('cursor','pointer');
+			 },
+			 onRegionOut: function(event,label){
+				 $(this).css('cursor','default');
+			 }
+	        });
+	      });
+	      //@code_end
+	    </script>
     </div>
 </div>
+
 <div class="row">
     <div class="span6">
-    <!-- Timeline of Probes -->
+    	<!-- Timeline of Probes -->
         <h3>Timeline of Probes</h3>
+  		<?php if (count($countrycountdates) > 0) { ?>
         <canvas id="darknetCountryChart" height="300" width="550"></canvas>
-        <script>
+        <script type="text/javascript">
         $(document).ready(function(){
-            //var displayLabels = document.getElementById('darknetSummaryChartLabels').textContent.split(",");
-            //var dataPoints = document.getElementById('darknetSummaryChartData').textContent.split(",");
-        
             var data = {
                 labels : [<?php print "'" . implode("','", $datelabels) . "'";?>],
                 datasets : [ 
@@ -196,6 +208,9 @@ to perform automated scans of hosts.</p>
             });        
         });
         </script>
+		<?php } else { ?>
+			No probes tracked.
+		<?php } ?>
     </div>
     <div class="span6">
     <!-- Tag Cloud -->
--- html/js/darknetSummaryChart.js
+++ html/js/darknetSummaryChart.js
@@ -1,20 +1,22 @@
 $(document).ready(function(){
-	var displayLabels = document.getElementById('darknetSummaryChartLabels').textContent.split(",");
-	var dataPoints = document.getElementById('darknetSummaryChartData').textContent.split(",");
-
-    var data = {
-        labels : displayLabels,
-        datasets : [ 
-            { 
-            fillColor : "rgba(200,220,160,0.2)",
-            strokeColor : "rgba(200,220,160,1)",
-            data : dataPoints
-            }
-        ]
-    }
-    var myNewChart = new Chart(document.getElementById("darknetChart").getContext("2d")).Bar(data);
-	$("#darknetChart").click(function (evt) {
-		var activeBars = myNewChart.getBarsAtEvent(evt);
-		location.href="?action=reports&report=by_port&ports=" + activeBars[0]["label"];
-	});        
+	if (document.getElementById('darknetSummaryChartLabels') !== null) {
+		var displayLabels = document.getElementById('darknetSummaryChartLabels').textContent.split(",");
+		var dataPoints = document.getElementById('darknetSummaryChartData').textContent.split(",");
+	
+	    var data = {
+	        labels : displayLabels,
+	        datasets : [ 
+	            { 
+	            fillColor : "rgba(200,220,160,0.2)",
+	            strokeColor : "rgba(200,220,160,1)",
+	            data : dataPoints
+	            }
+	        ]
+	    }
+	    var myNewChart = new Chart(document.getElementById("darknetChart").getContext("2d")).Bar(data);
+		$("#darknetChart").click(function (evt) {
+			var activeBars = myNewChart.getBarsAtEvent(evt);
+			location.href="?action=reports&report=by_port&ports=" + activeBars[0]["label"];
+		});     
+	}
 })  

67c1978e7c19f82385e16b6a094a540d221f2832
Justin C. Klein Keane <jukeane@sas.upenn.edu>
2014-11-06 14:25:16 -0500

    Added date field. Todo: datepicker not working, date not added to upload.

--- app/actions/upload_nessus_csv.php
+++ app/actions/upload_nessus_csv.php
@@ -15,9 +15,27 @@ if (isset($_FILES['nessus_csv'])) {
         $message = 'File successfully imported.';
     }
     else {
-    	$message = 'There was a problem with the import!';
+    	$errors = array(
+	        0=>"There is no error, the file uploaded with success",
+	        1=>"The uploaded file exceeds the upload_max_filesize directive in php.ini",
+	        2=>"The uploaded file exceeds the MAX_FILE_SIZE directive that was specified in the HTML form",
+	        3=>"The uploaded file was only partially uploaded",
+	        4=>"No file was uploaded",
+	        6=>"Missing a temporary folder"
+		); 
+    	$message = 'There was a problem with the import! ' . $errors[$_FILES['nessus_csv']['error']];
     }
 }
+
+$form_name='upload_nessus_csv';
+$form = new Form();
+$form->set_name($form_name);
+$token = $form->get_token();
+$form->save();
+
+
+hector_add_js('bootstrap-datepicker.js');
+hector_add_css('datepicker.css');
 
 include_once($templates. 'admin_headers.tpl.php');
 include_once($templates . 'upload_nessus_csv.tpl.php');
--- app/templates/upload_nessus_csv.tpl.php
+++ app/templates/upload_nessus_csv.tpl.php
@@ -5,7 +5,26 @@
 <p id='explaination'>After running a Nessus scan you can export the results as a Comma Separated Value (CSV) file.
 HECTOR can import these files, creating host records as necessary, and appending vulnerabilities and descriptions 
 to HECTOR records.</p>
-<form method="post" action="?action=upload_nessus_csv" enctype="multipart/form-data">
-<input type="file" name="nessus_csv"/>
+<form method="post" action="?action=upload_nessus_csv" enctype="multipart/form-data" name="upload_nessus_csv">
+File: <input type="file" name="nessus_csv"/><br/>
+Date: <div class="input-append date" id="dp" data-date="<?php echo date('Y-m-d');?>" data-date-format="yyyy-mm-dd">
+		<input name='scan_date' data-format="yyyy-MM-dd hh:mm:ss" id='scan_date' type="text" value=""/>
+		<span class="add-on"><i class="icon-calendar"></i></span>
+		</div><br/>
 <input type="submit" value="Upload CSV"/>
+<input type="hidden" name="token" value="<?php echo $token;?>"/>
+<input type="hidden" name="form_name" value="<?php echo $form_name;?>"/>
 </form>
+<script type="text/javascript">
+	var ddatetime = new Date(<?php echo strtotime($vuln_detail->get_datetime());?> *1000);
+	var defaulttime = '<?php echo date(' H:i:s');?>';
+    $('#dp').datepicker().on('changeDate', function(ev){
+    	if (ev.date.valueOf() > ddatetime) {
+				document.getElementById('scan_date').value += defaulttime;
+    	}
+    	else {
+    		document.getElementById('scan_date').value='0000-00-00 00:00:00';
+    		alert('Invalid Scan Time');
+    	}
+    });
+</script>

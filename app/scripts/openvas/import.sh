64a5aaefe09063f2499ff3da222ed6a8e13f8ea2
Justin C. Klein Keane <justin@madirish.net>
2016-04-27 09:23:16 -0400

    Fixed nomenclature and documentation

--- app/scripts/openvas/import.sh
+++ app/scripts/openvas/import.sh
@@ -1,5 +1,9 @@
 #!/bin/bash
-for importfile in *.toimport; do 
-  php /opt/hector/app/scripts/openvas/import.php "$importfile"
-  mv "$importfile" "$importfile.done"
-done
+count=`ls -1 /opt/hector/app/scripts/openvas/*.toimport 2>/dev/ull | wc -l`
+if [ $count != 0 ]
+then
+  for importfile in /opt/hector/app/scripts/openvas/*.toimport; do 
+    php /opt/hector/app/scripts/openvas/import.php "$importfile"
+    mv "$importfile" "$importfile.done"
+  done
+fi

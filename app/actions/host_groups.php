bfce3d993c6f5cf4caef576cbc3eab2d136cba0e
Justin C. Klein Keane <justin@madirish.net>
2016-05-13 14:34:33 -0400

    Tabs on host group display and OS breakdown

--- app/actions/host_groups.php
+++ app/actions/host_groups.php
@@ -13,6 +13,14 @@ require_once($approot . 'lib/class.Host.php');
 require_once($approot . 'lib/class.Host_group.php');
 require_once($approot . 'lib/class.Collection.php');
 
+function randColor($n) {
+	$retval = "";
+	for ($i=0; $i<$n; $i++) {
+		$retval .= '"#' . dechex(rand(0x000000, 0xFFFFFF)) . '",';
+	}
+	return $retval;
+}
+
 if (isset($_POST['hostgroup'])) {
 	// Adding hosts from the search form
     $hostgroup = new Host_group($_POST['hostgroup']);
@@ -34,8 +42,26 @@ if (isset($_GET['host_group_id']) && isset($_GET['live'])) {
 	$hosts = array();
 	$hostgroup = new Host_group($_GET['host_group_id']);
 	$host_ids = $hostgroup->get_live_host_ids();
+	$oscounts = array('Unknown'=>0);
 	foreach ($host_ids as $host_id) {
-		$hosts[] = new Host($host_id);
+		$host = new Host($host_id);
+		$hosts[] = $host;
+		if (array_key_exists($host->get_os(), $oscounts)) {
+			$oscounts[$host->get_os()] ++; 
+		}
+		else {
+			if ($host->get_os() == '') $oscounts['Unknown']++;
+			else $oscounts[$host->get_os()] = 1;
+		}
+	}
+	$oscountlabels = '"' . implode('","', array_keys($oscounts)) . '"';
+	//$oscountdata = '"' . implode('","', array_values($oscounts)) . '"';
+	$oscountcolors = randColor(count($oscounts));
+	$oscountdata = "";
+	$oscounttips = "";
+	foreach ($oscounts as $key=>$val) {
+		$oscountdata .= "'" . $key . "':{'count':" . $val . ",'href':'?action=search&os=" . $key . "'},";
+		$oscounttips .= "' " . $key . " " . $val . "', ";
 	}
 	$prefix = "Live ";
 	$filter = '<a href="?action=host_groups&host_group_id=' . $hostgroup->get_id() . '">Show all</a>';
--- app/templates/host_groups.tpl.php
+++ app/templates/host_groups.tpl.php
@@ -1,9 +1,20 @@
+
+
 <?php if (isset($message)):?>
 	<div id="message" class="alert"><?php echo $message;?></div>
 <?php endif;?>
 
 <?php if (isset($_GET['host_group_id'])):?>
 	<h3><?php echo $prefix . " " . $hostgroup->get_name();?> Hostgroup Members    <small><?php echo $filter;?></small></h3>
+
+<ul class="nav nav-tabs" id="hostGroupTab">
+	<li class="active"><a href="#overview" data-toggle="tab">Overview</a></li>
+	<li><a href="#os" data-toggle="tab">OS Breakdown</a></li>
+</ul>
+
+<div class="tab-content">
+<div class="tab-pane active" id="overview">
+
 	<table id="hostgroupstable" class="table table-striped table-bordered">
 	<thead>
 		<tr>
@@ -26,6 +37,68 @@
 		<?php endforeach;?>
 	</tbody>
 	</table>
+	
+</div>
+<div class="tab-pane" id="os">
+	<div id="osVersions-div" class="chart-container">
+		<div id="osVersionsChartLegend"></div>
+		<canvas id="osVersions" widht="300"></canvas>
+	</div>
+</div>
+</div>
+	
+	<script type="text/javascript">
+		function legend(parent, data) {
+		    parent.className = 'chart-legend';
+		    var datas = data.hasOwnProperty('datasets') ? data.datasets : data;
+	
+		    datas.forEach(function(d) {
+		        var title = document.createElement('span');
+		        title.className = 'title';
+		        title.style.borderColor = d.hasOwnProperty('strokeColor') ? d.strokeColor : d.color;
+		        title.style.borderStyle = 'solid';
+		        parent.appendChild(title);
+	
+		        var text = document.createTextNode(d.title);
+		        title.appendChild(text);
+		    });
+		}
+	
+		$(document).ready(function(){
+			var pieChartId = 'osVersions';
+			var chartLabels = [<?php echo $oscountlabels; ?>];
+			var chartData = {<?php echo $oscountdata; ?>};
+			var colors = [<?php echo $oscountcolors; ?>];
+			var data = [];
+			
+			for (var i = 0; i < chartLabels.length; i++){
+				var label = chartLabels[i];
+				var count = chartData[label]['count'];
+				data.push({value:count , color:colors[i] ,title:label + " - " + count});
+			}
+			
+			var ctx = document.getElementById(pieChartId).getContext("2d");
+			ctx.canvas.width = 300;
+			ctx.canvas.height = 300;
+			var incidentChart = new Chart(ctx).Pie(data, {
+					animation:false, 
+					percentageInnerCutout : 0,
+					segmentStrokeColor: "#000",
+					segmentStrokeWidth: 1,
+					});
+			var pieChartLegend = document.getElementById('osVersionsChartLegend');
+			legend(pieChartLegend,data);
+		
+			// Hyperlink the legend items
+			var i = 0;
+			var titleTarget = "#osVersionsChartLegend .title";
+			$(titleTarget).each(function(index){
+				$(this).attr('onclick', 'location.href="' + chartData[chartLabels[i]]['href'] + '"');
+				i++;
+			});
+		});
+		
+	</script>
 <?php else:?>
 	<h3>Host Groups</h3>
 	<table id="hostgroupstable" class="table table-striped table-bordered">
@@ -54,3 +127,4 @@
 	</tbody>
 	</table>
 <?php endif;?>
+

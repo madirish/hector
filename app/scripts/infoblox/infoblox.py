cbe47c51466076d392ea6f09409f688831e09f53
Josh Bauer <bauerj@mlhs.org>
2016-08-30 16:30:36 -0400

    removing php import and switching to a faster python script

--- app/scripts/infoblox/infoblox.py
+++ app/scripts/infoblox/infoblox.py
@@ -10,6 +10,14 @@ sys.path.append(appPath + "/lib/pylib")
 from pull_config import Configurator
 configr = Configurator()
 
+# Credentials used for the database connection
+configr = Configurator()
+DB = configr.get_var('db')
+HOST = configr.get_var('db_host')
+USERNAME = configr.get_var('db_user')
+PASSWORD = configr.get_var('db_pass')
+
+
 #logging set up
 logger = logging.getLogger('Infoblox python script')
 hdlr = logging.FileHandler(appPath + '/logs/message_log')
@@ -27,17 +35,34 @@ logger.debug('args: [\''+('\', \''.join(sys.argv))+'\']')
 
 #config vars
 infoblox_dir = configr.get_var('approot')+"app/scripts/infoblox/"
-infoblox_log_file_name = ""
+infoblox_log_file_name = "infoblox.log.1.gz"
 output_filename = "infoblock-queries.txt"
 
 #pull log file
 #@TODO: set up a method of pulling the infoblox log file
 
-outfile = open(infoblox_dir+output_filename, "w")
+#outfile = open(infoblox_dir+output_filename, "w")
 
 gzip = subprocess.Popen(["gzip", "-dc", infoblox_dir+"logs/"+infoblox_log_file_name],stdout=subprocess.PIPE)
 grep = subprocess.Popen(["grep", "-e", "query"],stdin=gzip.stdout,stdout=subprocess.PIPE)
 awk = subprocess.Popen(["awk", "-f", infoblox_dir+"filter.awk"],stdin=grep.stdout,stdout=subprocess.PIPE)
 sort = subprocess.Popen(["sort"],stdin=awk.stdout, stdout=subprocess.PIPE)
-uniq = subprocess.Popen(["uniq"],stdin=sort.stdout,stdout=outfile)
-uniq.communicate() # wait for process to complete
+uniq = subprocess.Popen(["uniq"],stdin=sort.stdout,stdout=subprocess.PIPE)            # outfile)
+records = uniq.communicate()[0] # wait for process to complete
+
+logger.info("Starting to import records into database")
+count = 0;
+conn = MySQLdb.connect(host=HOST,
+      user=USERNAME,
+      passwd=PASSWORD,
+      db=DB)
+cursor = conn.cursor()
+
+for r in records:
+    count+=1
+    
+    if count%1000==0:
+        logger.info(str(count)+' records added')
+        
+        
+logger.info("infoblox.py finished. "+str(count)+" records added.")

a6cc7f21134fd5476d39dacb1add76d270121975
Josh Bauer <bauerj@mlhs.org>
2016-09-16 13:13:34 -0400

    added default date to yesterdays date if no date is supplied. added environment vars for aws auth.

--- app/scripts/opendns/opendns.py
+++ app/scripts/opendns/opendns.py
@@ -3,6 +3,8 @@
 import subprocess
 import logging
 import os,sys
+from datetime import date, timedelta
+import os
 
 appPath = os.path.abspath(os.path.dirname(os.path.realpath(__file__)) + "/../../")
 sys.path.append(appPath + "/lib/pylib")
@@ -20,7 +22,7 @@ error_hdlr.setFormatter(formatter)
 error_hdlr.setLevel(logging.ERROR)
 logger.addHandler(hdlr) 
 logger.addHandler(error_hdlr)
-logger.setLevel(logging.INFO)
+logger.setLevel(logging.DEBUG)
 
 logger.info('opendns.py starting')
 logger.debug('args: [\''+('\', \''.join(sys.argv))+'\']')
@@ -28,18 +30,32 @@ logger.debug('args: [\''+('\', \''.join(sys.argv))+'\']')
 #config vars
 opendns_dir = configr.get_var('approot')+"app/scripts/opendns/"
 bucket = configr.get_var('s3_bucket')
-if len(sys.argv)<2:
-	logger.error('requires date as param YYYY-MM-DD. script terminated.')
-	exit(1)
-target_date = sys.argv[1]
+if len(sys.argv)>=2:
+	target_date = sys.argv[1]
+	logger.info('User supplied target date \''+target_date+'\'')
+else:
+	target_date = str(date.today() - timedelta(days=1))
+	logger.info('There was no user supplied target date. Using target date \''+target_date+'\'')
+
+
 output_filename = "Blocked-Domains-"+target_date+".txt"
 logger.debug('opendns_dir: '+opendns_dir)
 logger.debug('target_date: '+target_date)
 logger.debug('output_filename: '+output_filename)
 
+aws_env = os.environ.copy()
+aws_env['AWS_ACCESS_KEY_ID'] = configr.get_var('aws_access_key_id')
+aws_env['AWS_SECRET_ACCESS_KEY'] = configr.get_var('aws_secret_access_key')
 logger.info('pulling logs from s3 bucket.')
-aws = subprocess.Popen(["aws", "s3", "cp", "--recursive", "s3://"+bucket+"/dnslogs/"+target_date+"/", opendns_dir+"logs/"+target_date+"/"], stdout=subprocess.PIPE)
-aws.communicate() # wait for process to complete
+aws = subprocess.Popen(["aws", "s3", "cp", "--recursive", "s3://"+bucket+"/dnslogs/"+target_date+"/", opendns_dir+"logs/"+target_date+"/"], 
+					stdout=subprocess.PIPE,stderr=subprocess.PIPE, env=aws_env)
+aws_out,aws_err = aws.communicate() # wait for process to complete
+if aws.returncode>0:
+	logger.error('AWS process error. Exited with status code: \''+str(aws.returncode)+'\'')
+	logger.error('AWS process error. Error output: \''+str(aws_err).strip()+'\'')
+	exit(1);
+else:
+	logger.debug('AWS completed with return code: \''+str(aws.returncode)+'\'')
 
 outfile = open(opendns_dir+output_filename, "w")
 

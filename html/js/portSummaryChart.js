467b3412e0b515eb35393b6fb939f159aa41809d
Justin C. Klein Keane <justin@madirish.net>
2016-10-24 13:40:50 -0400

    Upgraded to Chart.js version 2.3.0

--- html/js/portSummaryChart.js
+++ html/js/portSummaryChart.js
@@ -4,18 +4,22 @@ $(document).ready(function(){
 		var dataPoints = document.getElementById('portSummaryChartData').textContent.split(",");
 	
 	    var data = {
+	    	type: 'horizontalBar',
+	    	data: {
 	        labels : displayLabels,
-	        datasets : [ 
-	            { 
+	        datasets : [{ 
 	            fillColor : "rgba(120,220,220,0.2)",
 	            strokeColor : "rgba(120,220,220,1)",
+	            borderColor : "rgba(0,0,0,1)",
+	            borderWidth: 1,
 	            data : dataPoints
-	            }
-	        ]
+	            }]
+	    	},
+	    	options: { legend: {display: false}, borderColor: '#000' }
 	    }
-	    var myNewChart = new Chart(document.getElementById("topPortsChart").getContext("2d")).Bar(data);
+	    var portSummaryChart = new Chart($("#topPortsChart"), data);
 		$("#topPortsChart").click(function (evt) {
-			var activeBars = myNewChart.getBarsAtEvent(evt);
+			var activeBars = portSummaryChart.getBarsAtEvent(evt);
 			location.href="?action=reports&report=by_port&ports=" + activeBars[0]["label"];
 		});
 	}

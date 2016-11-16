968d8240a52d784ccf7101b8dc53eb06f8988cd1
Justin C. Klein Keane <justin@madirish.net>
2016-11-16 10:35:26 -0500

    Fixed broken table display

--- html/js/darknetSummaryChart.js
+++ html/js/darknetSummaryChart.js
@@ -1,23 +1,28 @@
 $(document).ready(function(){
-	if (document.getElementById('darknetSummaryChartLabels') !== null) {
-		var displayLabels = document.getElementById('darknetSummaryChartLabels').textContent.split(",");
-		var dataPoints = document.getElementById('darknetSummaryChartData').textContent.split(",");
+    if (document.getElementById('darknetSummaryChartLabels') !== null) {
+        var displayLabels = document.getElementById('darknetSummaryChartLabels').textContent.split(",");
+        var dataPoints = document.getElementById('darknetSummaryChartData').textContent.split(",");
+        
+        var data = {
+                labels : displayLabels,
+                datasets : [{ 
+                    //fillColor : "rgba(200,220,160,0.2)",
+                    //strokeColor : "rgba(200,220,160,1)",
+                    data : dataPoints,
+                }],
+                borderColor: "#000000",
+        };
+        var options = {
+                legend: {
+                    display: false,
+                }
+        };
 	
-	    var data = {
-	    	type: 'bar',
-	        labels : displayLabels,
-	        datasets : [ 
-	            { 
-	            fillColor : "rgba(200,220,160,0.2)",
-	            strokeColor : "rgba(200,220,160,1)",
-	            data : dataPoints
-	            }
-	        ]
-	    }
-	    var myNewChart = new Chart($("darknetChart"), data);
-		$("#darknetChart").click(function (evt) {
-			var activeBars = myNewChart.getBarsAtEvent(evt);
-			location.href="?action=reports&report=by_port&ports=" + activeBars[0]["label"];
-		});     
-	}
-})  
+        var specs = {
+            type: 'bar',
+            data: data,
+            options: options
+         };
+         var darknetSummaryChart = new Chart($("#darknetChart"), specs);
+     }
+});  

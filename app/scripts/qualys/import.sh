dc6cde04cb8a48b1494eda2061094982ea6c55fd
Justin C. Klein Keane <justin@madirish.net>
2016-12-21 15:37:50 -0500

    Re-ordered bash script to prevent multiple imports for long running jobs

--- app/scripts/qualys/import.sh
+++ app/scripts/qualys/import.sh
@@ -5,7 +5,7 @@ count=`ls -1 /opt/hector/app/scripts/qualys/*.toimport 2>/dev/ull | wc -l`
 if [ $count != 0 ]
 then
   for importfile in /opt/hector/app/scripts/qualys/*.toimport; do 
-    /usr/bin/php /opt/hector/app/scripts/qualys/import.php "$importfile"
     mv "$importfile" "$importfile.done"
+	/usr/bin/php /opt/hector/app/scripts/qualys/import.php "$importfile.done"
   done
 fi
